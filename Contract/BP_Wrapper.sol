157213362026b0dc525cecd4f295a48b4064b71a
CT Smyth <cliff.smyth@gmail.com>
2020-04-08 15:31:26 -0400

    Auto stash before merge of "BulletProof_0_2" and "origin/BulletProof_0_2"

---
+++ Contract/BP_Wrapper.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.6.0;
+ 
+ import "./BulletProof_0_2_0.sol";
+ 
+ contract Wrapper is BulletProof {
+    /**
+     * @dev Wrapper for create new record
+     */
+     
+    function NEW_RECORD (uint256 idx, string calldata reg, uint8 stat) external payable{
+        newRecord(idx, keccak256(abi.encodePacked(reg)), stat);
+    }
+
+    /**
+     * @dev Wrapper for comparing records
+     */
+     
+    function COMPARE_REGISTRANT (uint256 idx, string calldata reg) external view returns(string memory) {
+         
+        if (keccak256(abi.encodePacked(reg)) == database[idx].registrant){
+            return "Registrant match confirmed";
+        } else {
+            return "Registrant does not match";
+        }
+    }
+
+    /**
+     * @dev Wrapper for force changing record status
+     */
+    
+    function FORCE_MOD_STATUS(uint idx, uint8 stat) external payable{
+        forceModifyStatus(idx,stat);
+    }
+    
+    /**
+     * @dev Wrapper for force changing the record without tests
+     */
+     
+    function FORCE_MOD_REGISTRANT (uint256 idx, string calldata reg) external payable {
+        modifyRegistrant(idx, keccak256(abi.encodePacked(reg)));
+    }
+    
+    
+    /**
+     * @dev Wrapper for changing record status with tests
+     */
+    
+    function MOD_STATUS(uint idx, string calldata regstrnt, uint8 stat) external payable{
+        modifyStatus(idx,keccak256(abi.encodePacked(regstrnt)),stat);
+    }
+
+    /**
+     * @dev Wrapper for Asset transfer with tests
+     */
+    
+    function TRANSFER_ASSET (uint256 idx, string memory oldreg, string memory newreg, uint8 newstat) public payable {
+        transferAsset(idx, keccak256(abi.encodePacked(oldreg)), keccak256(abi.encodePacked(newreg)),newstat);
+     }
+
+
+     /**
+     * @dev Wrapper for automated Asset transfer with tests
+     */
+
+    function PRIVATE_SALE (uint256 idx, string memory oldreg, string memory newreg, uint8 newstat) public payable {
+        robotTransferAsset(idx, keccak256(abi.encodePacked(oldreg)), keccak256(abi.encodePacked(newreg)),newstat);
+     }
+}
