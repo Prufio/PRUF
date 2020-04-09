6137dd6e887be74089bfbbe1c22db5464fd46b31
Unknown <drakesmyth@gmail.com>
2020-04-09 15:25:29 -0400

    Fixed C/P error

--- Contract/BP_Frontend.sol
+++ Contract/BP_Frontend.sol
@@ -1,147 +1,343 @@
 pragma solidity ^0.6.0;
- 
- import "./BulletProof_0_2_1.sol";
- import "./PullPayment.sol";
- import "./SafeMath.sol";
- 
- contract BP_Frontend is BulletProof, PullPayment {
-    using SafeMath for uint256;
-    
-    uint internal costUnit = 0.01 ether;
-    uint internal minEscrowAmount = 0.1 ether;
-    address internal mainWallet;
-    
-    /**
-     * @dev Set contract parameters
-     */
-    function setWallet (address _addr) public onlyOwner {
-        mainWallet = _addr;
-    }
-   
-    function setCost (uint _cost) public onlyOwner {
-        costUnit = _cost;
-    }
-   
-    function setEscrow (uint _escrow) public onlyOwner {
-        minEscrowAmount = _escrow;
-    }
-   
-    /**
-     * @dev Wrapper for admin lock record
-     */
-    function ADMIN_LOCK (uint idx) public {
-        adminLock(keccak256(abi.encodePacked(idx)));
-    }
-    
-      /**
-     * @dev Wrapper for admin unlock record
-     */
-    function ADMIN_UNLOCK (uint idx) public {
-        adminUnlock(keccak256(abi.encodePacked(idx)));
-    }
-    
+
+/**
+ * @title BulletProof
+ * @dev Store & retreive a record
+ * Need to explore the implications of registering with serial only and reregistering with serial+secret
+ * 
+ * Authorization for registry changes from adress -> uint mapping?
+ * 
+ * Record status field key
+ * 0 = no status, transferrable
+ * 1 = transferrable
+ * 2 = nontransferrable
+ * 3 = stolen
+ * 4 = lost
+ * 255 = record locked (contract will not modify record without this first being unlocked by origin)
+ * 
+ * RegisteredUsers:
+ * 0 = addressHash not authorized 
+ * > 0 Authorized for database functions
+ * 9 = authorized for automation function
+ * 
+ * 
+ * A Status 5 authorizes private sales, in which an point of sale app can verify that the "seller" can verify his/her
+ * registrant-hash in-app and allow transfer through our robot registrar to a party specified in-app. Implementation TBD
+ * 
+ * 
+ */
+
+import "./BP_Storage.sol";
+
+contract BulletProof is Storage {
     
     /**
-     * @dev Wrapper for create new record
+     * @dev Authorize / Deauthorize / Authorize automation for an address be permitted to make record modifications
+     * ----------------INSECURE -- keccak256 of address must be generated clientside in release.
+     * something like:
+     * 
+     * function authorize(bytes32 authAddrHash) public onlyOwner {
+     *   registeredUsers[authAddrHash] = 1;
+     * }
      */
-    function NEW_RECORD (uint idx, string memory reg, uint8 stat) public payable {
-        deductPayment(1);
-        newRecord(msg.sender, keccak256(abi.encodePacked(idx)), keccak256(abi.encodePacked(reg)), stat);
+    function authorize(address authAddr) public onlyOwner {
+        bytes32 hash;
+        hash = keccak256(abi.encodePacked(authAddr));
+        registeredUsers[hash] = 1;
+    }
+    
+    function authorizeRobot(address authAddr) public onlyOwner {
+        bytes32 hash;
+        hash = keccak256(abi.encodePacked(authAddr));
+        registeredUsers[hash] = 9;
+    }
+    
+    function deauthorize(address authAddr) public onlyOwner {
+        bytes32 hash;
+        hash = keccak256(abi.encodePacked(authAddr));
+        registeredUsers[hash] = 0;
+    }
+    
+    
+     /**
+     * @dev Administrative lock a database entry at index idx
+     */
+    function adminLock(uint idx) public onlyOwner {
+        
+        require(
+            database[idx].status != 255 ,
+            "Record already locked"
+        );
+        
+        database[idx].status = 255;
+        database[idx].registrar = keccak256(abi.encodePacked(msg.sender));
+    }
+    
+    
+    /**
+     * @dev Administrative unlock a database entry at index idx
+     */
+    function adminUnLock(uint idx) public onlyOwner {
+        
+        require(
+            database[idx].status == 255 ,
+            "Record not locked"
+        );
+        
+        database[idx].status = 2;            // set to notransferrable on unlock????????????????????!!!!!!!!!!!!!!!!!
+        database[idx].registrar = keccak256(abi.encodePacked(msg.sender));
     }
 
-    
+
     /**
-     * @dev Wrapper for changing record status with tests
+     * @dev Store a complete record at index idx
      */
-    function MOD_STATUS(uint idx, string memory regstrnt, uint8 stat) public payable {
-        deductPayment(1);
-        modifyStatus(msg.sender, keccak256(abi.encodePacked(idx)),keccak256(abi.encodePacked(regstrnt)),stat);
+    function newRecord(address sender, uint idx, bytes32 regstrnt, uint8 stat) internal { //public
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 1 ,
+            "Address not authorized"
+        );
+        require(
+            database[idx].registrant == 0 ,
+            "Record already exists"
+        );
+        require(
+            regstrnt != 0 ,
+            "Registrant cannot be empty"
+        );
+        require(
+            stat != 255 ,
+            "creation of locked record prohibited"
+        );
+        
+        database[idx].registrar = keccak256(abi.encodePacked(sender));
+        database[idx].registrant = regstrnt;
+        database[idx].status = stat;
     }
 
 
     /**
-     * @dev Wrapper for Asset transfer with tests
+     * @dev force modify record field 'status' at index idx
      */
-    function TRANSFER_ASSET (uint idx, string memory oldreg, string memory newreg, uint8 newstat) public payable {
-        deductPayment(1);
-        transferAsset(msg.sender, keccak256(abi.encodePacked(idx)), keccak256(abi.encodePacked(oldreg)), keccak256(abi.encodePacked(newreg)),newstat);
+    function modifyStatus(address sender, uint idx, bytes32 regstrnt, uint8 stat) internal {
+        require(
+            database[idx].registrant == regstrnt,
+            "records do not match - status change aborted"
+        );
+        if (registeredUsers[keccak256(abi.encodePacked(sender))] == 9){
+            robotModifyStatus(sender,idx,stat);
+        } else {
+            forceModifyStatus(sender,idx,stat);
+        }
+    }
+    
+    
+    /**
+     * @dev modify record field 'status' at index idx
+     */
+    function forceModifyStatus(address sender, uint idx, uint8 stat) internal {
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 1,
+            "Address not authorized"
+        );
+        require(
+            database[idx].registrant != 0 ,
+            "No Record exists to modify"
+        );
+        require(
+            database[idx].status != 255 ,
+            "Record locked"
+        );
+        require(
+            stat != 255 ,
+            "locking by user prohibited"
+        );
+    
+        if(  stat == database[idx].status) {
+            return;                         //save gas?
+        }
+        
+        database[idx].registrar = keccak256(abi.encodePacked(sender));
+        database[idx].status = stat;
+    }
+    
+    
+    /**
+     * @dev robot modify record field 'status' at index idx
+     */
+    function robotModifyStatus(address sender, uint idx, uint8 stat) internal {
+        
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 9,
+            "RMS: Address not authorized for automation"
+        );
+        require(
+            database[idx].registrant != 0 ,
+            "No Record exists to modify"
+        );
+        require(
+            database[idx].status != 255 ,
+            "Record locked"
+        );
+        require(
+            stat != 255 ,
+            "locking by user prohibited"
+        );
+    
+        if(  stat == database[idx].status) {
+            return;                         //save gas?
+        }
+        
+        database[idx].registrar = keccak256(abi.encodePacked(sender));
+        database[idx].status = stat;
+    }
+    
+    
+    /**
+     * @dev modify record field 'registrant' at index idx
+     */
+    function modifyRegistrant(address sender, uint idx, bytes32 regstrnt) internal { //public
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 1  ,
+            "Address not authorized"
+        );
+        require(
+            database[idx].registrant != 0 ,
+            "No Record exists to modify"
+        );
+        require(
+            database[idx].status != 255 ,
+            "Record locked"
+        );
+        require(
+            database[idx].status != 2 ,
+            "Asset marked nontransferrable"
+        );
+        require(
+            database[idx].status != 3 ,
+            "Asset reported stolen"
+        );
+        require(
+            database[idx].status != 4 ,
+            "Asset reported lost"
+        );
+        require(
+            (database[idx].status == 0) || (database[idx].status == 1) ,
+            "Tranfer prohibited"
+        );
+        require(
+            regstrnt != 0 ,
+            "Registrant cannot be empty"
+        );
+        require(
+            database[idx].registrant != regstrnt ,
+            "New record is identical to old record"
+        );
+        
+        database[idx].registrar = keccak256(abi.encodePacked(sender));
+        database[idx].registrant = regstrnt;
+    }
+    
+    
+    /**
+     * @dev robot modify record field 'registrant' at index idx
+     */
+    function robotModifyRegistrant(address sender, uint idx, bytes32 regstrnt) internal { //public
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 9 ,
+            "RMR: Address not authorized for auttomation"
+        );
+        require(
+            database[idx].registrant != 0 ,
+            "No Record exists to modify"
+        );
+        require(
+            database[idx].status != 255 ,
+            "Record locked"
+        );
+        require(
+            database[idx].status != 2 ,
+            "Asset marked nontransferrable"
+        );
+        require(
+            database[idx].status != 3 ,
+            "Asset reported stolen"
+        );
+        require(
+            database[idx].status != 4 ,
+            "Asset reported lost"
+        );
+        require(
+            (database[idx].status == 0) || (database[idx].status == 1) ,
+            "Tranfer prohibited"
+        );
+        require(
+            regstrnt != 0 ,
+            "Registrant cannot be empty"
+        );
+        require(
+            database[idx].registrant != regstrnt ,
+            "New record is identical to old record"
+        );
+        
+        database[idx].registrar = keccak256(abi.encodePacked(sender));
+        database[idx].registrant = regstrnt;
+    }
+
+
+     /**
+     * @dev modify record with test for match to old record
+     */
+    function transferAsset (address sender, uint idx, bytes32 oldreg, bytes32 newreg, uint8 newstat) internal {
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 1  ,
+            "Address not authorized"
+        );
+        require(
+            database[idx].registrant == oldreg ,
+            "Records do not match - record change aborted"
+        );
+        
+        modifyRegistrant(sender, idx, newreg);
+        forceModifyStatus(sender, idx, newstat);
+     
+     }
+     
+     
+    /**
+     * @dev Automation modify record with test for match to old record
+     */
+    function robotTransferAsset (address sender, uint idx, bytes32 oldreg, bytes32 newreg, uint8 newstat) internal {
+        require(
+            registeredUsers[keccak256(abi.encodePacked(sender))] == 9 ,
+            "RTA: Address not authorized for automation"
+        );
+        require(
+            database[idx].registrant == oldreg ,
+            "Records do not match - record change aborted"
+        );
+        
+        robotModifyRegistrant(sender, idx, newreg);
+        robotModifyStatus(sender, idx, newstat);
+     
      }
 
 
     /**
-     * @dev Wrapper for automated Asset transfer with tests
-     */
-    function PRIVATE_SALE (uint idx, string memory oldreg, string memory newreg, uint8 newstat) public payable {
-        deductPayment(1);
-        robotTransferAsset(msg.sender, keccak256(abi.encodePacked(idx)), keccak256(abi.encodePacked(oldreg)), keccak256(abi.encodePacked(newreg)),newstat);
-    }
-    
-    
-    /**
-     * @dev Wrapper for force changing record status
-     */
-    function FORCE_MOD_STATUS(uint idx, uint8 stat) public payable {
-        deductPayment(5);
-        forceModifyStatus(msg.sender, keccak256(abi.encodePacked(idx)),stat);
-    }
-
-    
-    /**
-     * @dev Wrapper for force changing the record without tests
-     */
-    function FORCE_MOD_REGISTRANT (uint idx, string memory reg) public payable {
-        deductPayment(5);
-        modifyRegistrant(msg.sender, keccak256(abi.encodePacked(idx)), keccak256(abi.encodePacked(reg)));
-    }
-    
-    
-    /**
-     * @dev Wrapper for comparing records
-     */
-    function COMPARE_REGISTRANT (uint idx, string calldata reg) external view returns(string memory) {
-         
-        if (keccak256(abi.encodePacked(reg)) == database[keccak256(abi.encodePacked(idx))].registrant) {
-            return "Registrant match confirmed";
-        } else {
-            return "Registrant does not match";
-        }
-    }
-    
-    /**
      * @dev Return complete record from datatbase at index idx
      */
-    function RETRIEVE_RECORD (uint idx) external view returns (bytes32,bytes32,uint8) {
-        bytes32 idxHash = keccak256(abi.encodePacked(idx));
-        return (database[idxHash].registrar,database[idxHash].registrant,database[idxHash].status);
+    function retrieveRecord (uint idx) public view returns (bytes32,bytes32,uint8) {
+        return (database[idx].registrar,database[idx].registrant,database[idx].status);
     }
 
+    /** Funtions for plaintext contract interaction
+     * ------------------------------------------------------------------------------------------------------------------------------------------------
+     * newRecord
+     * modifyRegistrant
+     * 
+     * Additional wrappers:
+     * registrant compare / verify registrant hash
+     * 
+     */
     
-     /**
-     * @dev Deduct payment and transfer cost, call to PullPayment with msg.sender  *****MAKE pullPayment internal!!!! SECURITY
-     */ 
-    function WITHDRAW() public virtual payable {
-        withdrawPayments(msg.sender);
-    }
- 
- 
-    /**
-     * @dev Deduct payment and transfer cost, change to PullPayment
-     */   
-    function deductPayment (uint amount) private {
-        address _address = msg.sender;
-        uint messageValue = msg.value;
-        uint cost = amount.mul(costUnit);
-        uint change;
-        
-        //require (messageValue > 0, "Failed to Accept Payment: no txfr Balance"); 
-        require (messageValue >= costUnit.add(minEscrowAmount) ,
-            "Insufficient Eth"
-        );
-        
-        change = messageValue.sub(cost);
-        
-        _asyncTransfer(mainWallet, cost);
-        _asyncTransfer(_address, change);
-        
-    }
 }
