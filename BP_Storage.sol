f0043b2978ba64d6cff4a6da81bd8969603f5830
CT Smyth <cliff.smyth@gmail.com>
2020-04-11 11:21:18 -0400

    New variable names

--- BP_Storage.sol
+++ BP_Storage.sol
@@ -4,22 +4,14 @@ import "./Ownable.sol";
 
 contract Storage is Ownable {
     struct Record {
-        bytes32 registrar; // tokenID (or address) of registrant 
+        bytes32 registrar; // Address hash of registrant 
         bytes32 registrant;  // KEK256 Registered  owner
         uint8 status; // Status - Transferrable, locked, in transfer, stolen, lost, etc.
+        uint8 extra; // extra status for future expansion
         uint8 forceModCount; // Number of times asset has been forceModded.
-        uint8 assetClass; // Variable defines asset by distinctive class
         string description; // publically viewable asset description
     }
     
-    /*
-    struct IndexReference {
-        bytes32 assetClass;
-        bytes32 indexNum;
-        bytes32 make;
-        bytes32 model;
-    }
-    */
     
     // mapping(bytes32 => IndexReference) internal index;
     mapping(bytes32 => Record) internal database; //registry
