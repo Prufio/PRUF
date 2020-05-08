a204cffd1641b5440eaa9a51e11782422624638d
James B Smyth <jamesblaydesmyth@gmail.com>
2020-05-08 15:15:57 -0400

    Changes to test files, dynamic page jobs

--- Website/my-app/src/App.js
+++ Website/my-app/src/App.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import {useState} from 'react';
 import Tweet from './Tweet';
-
-function sendMessage(){
-  alert("I am a certified doctor");
-}
+import Main from './Main';
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-   
+  var [pageJob, setJob] = useState(1);
+
+  const changeJob = () => {
+    console.log("changing page job")
+    //alert("It's Changing!")
+    if (pageJob === 1){
+      setJob(0);
+    }
+    else if (pageJob === 0){
+      setJob(1);
+    }
+    //refreshPage();
   }
 
+  const refreshPage = () => {
+    window.location.reload(false);
+  }
 
   return (
     <div>
       <div className = 'header'>
         <h1>Twitter</h1>
-        <h2>Welcome to real twitter</h2>
-        <h3>This is actually twitter</h3>
-        <br>
-        </br>
-        <h4>Holy shit</h4>
+        <button onClick = {changeJob}>Toggle Page Job</button>
       </div>
+      <Main mode = {pageJob}/>
     <div className = 'app'>
-      <div className = 'increment'>
-          <h4 className = {isRed ? 'red' : ''}>Change My Color</h4>
-          <button onClick = {increment}>Increment</button>
-          <h2>{count}</h2>
-          <button onClick = {sendMessage}>Request Secret Message</button>
-      </div>
       <Tweet name = "@BeanStockpile663" content = "Who ate all of my beans? Life could not be worse." likes = "4.1k likes"/>
       <Tweet name = "@BeanChugger" content = "out of bean" likes = "98.2k likes"/>
-      <Tweet name = "@Mr_Fursuit" content = "Today I was arrested for having sex with a racoon. SMH." likes = "783 likes"/>
+      <Tweet name = "@Mr_Fursuit" content = "Yiff" likes = "783 likes"/>
     </div>
     </div>
   );
