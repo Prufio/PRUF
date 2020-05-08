718b68e2551e16e4fa4cc481313cb5a285148357
James B Smyth <jamesblaydesmyth@gmail.com>
2020-05-08 13:38:16 -0400

    Added test docs for react.js work, added main react pj

--- Website/my-app/src/App.js
+++ Website/my-app/src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Tweet from './Tweet';
+
+function sendMessage(){
+  alert("I am a certified doctor");
+}
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+   
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className = 'header'>
+        <h1>Twitter</h1>
+        <h2>Welcome to real twitter</h2>
+        <h3>This is actually twitter</h3>
+        <br>
+        </br>
+        <h4>Holy shit</h4>
+      </div>
+    <div className = 'app'>
+      <div className = 'increment'>
+          <h4 className = {isRed ? 'red' : ''}>Change My Color</h4>
+          <button onClick = {increment}>Increment</button>
+          <h2>{count}</h2>
+          <button onClick = {sendMessage}>Request Secret Message</button>
+      </div>
+      <Tweet name = "@BeanStockpile663" content = "Who ate all of my beans? Life could not be worse." likes = "4.1k likes"/>
+      <Tweet name = "@BeanChugger" content = "out of bean" likes = "98.2k likes"/>
+      <Tweet name = "@Mr_Fursuit" content = "Today I was arrested for having sex with a racoon. SMH." likes = "783 likes"/>
+    </div>
     </div>
   );
 }
