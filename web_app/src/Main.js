fd273006b09114d03d361a4ad34bf1721669374f
CT Smyth <cliff.smyth@gmail.com>
2020-05-14 11:47:36 -0400

    formatting

--- web_app/src/Main.js
+++ web_app/src/Main.js
@@ -16,51 +16,57 @@ class Main extends Component {
       <HashRouter>
         <div>
           <h1>
-            <img src={require("./BP Logo.png") } alt="Bulletproof Logo"/>
+            <img src={require("./BP Logo.png")} alt="Bulletproof Logo" />
           </h1>
-          <ul className="header">
-            <li>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/new-record">New Asset</NavLink>
-            </li>
-            <li>
-              <NavLink to="/retrieve-record">Look-up</NavLink>
-            </li>
-            <li>
-              <NavLink to="/transfer-asset">Transfer</NavLink>
-            </li>
-            <li>
-              <NavLink to="/modify-record-status">Status</NavLink>
-            </li>
-            <li>
-              <NavLink to="/decrement-counter">Countdown</NavLink>
-            </li>
-            <li>
-              <NavLink to="/modify-description">Description</NavLink>
-            </li>
-            <li>
-              <NavLink to="/add-note">Note</NavLink>
-            </li>
-            <li>
-              <NavLink to="/force-modify-record">Force Modify</NavLink>
-            </li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" component={Home} />
-            <Route path="/new-record" component={NewRecord} />
-            <Route path="/retrieve-record" component={RetrieveRecord} />
-            <Route path="/force-modify-record" component={ForceModifyRecord} />
-            <Route path="/transfer-asset" component={TransferAsset} />
-            <Route
-              path="/modify-record-status" component={ModifyRecordStatus}
-            />
-            <Route path="/decrement-counter" component={DecrementCounter} />
-            <Route path="/modify-description" component={ModifyDescription} />
-            <Route path="/add-note" component={AddNote} />
+          <div className="page">
+            <ul className="header">
+              <li>
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/new-record">New</NavLink>
+              </li>
+              <li>
+                <NavLink to="/retrieve-record">Look-up</NavLink>
+              </li>
+              <li>
+                <NavLink to="/transfer-asset">Transfer</NavLink>
+              </li>
+              <li>
+                <NavLink to="/modify-record-status">Status</NavLink>
+              </li>
+              <li>
+                <NavLink to="/decrement-counter">Countdown</NavLink>
+              </li>
+              <li>
+                <NavLink to="/modify-description">Description</NavLink>
+              </li>
+              <li>
+                <NavLink to="/add-note">Note</NavLink>
+              </li>
+              <li>
+                <NavLink to="/force-modify-record">Modify</NavLink>
+              </li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Home} />
+              <Route path="/new-record" component={NewRecord} />
+              <Route path="/retrieve-record" component={RetrieveRecord} />
+              <Route
+                path="/force-modify-record"
+                component={ForceModifyRecord}
+              />
+              <Route path="/transfer-asset" component={TransferAsset} />
+              <Route
+                path="/modify-record-status"
+                component={ModifyRecordStatus}
+              />
+              <Route path="/decrement-counter" component={DecrementCounter} />
+              <Route path="/modify-description" component={ModifyDescription} />
+              <Route path="/add-note" component={AddNote} />
+            </div>
           </div>
         </div>
       </HashRouter>
