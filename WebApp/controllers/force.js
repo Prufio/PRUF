034ba7d06c6121151d28fe6f2a8bfc437c14b9fa
CT Smyth <cliff.smyth@gmail.com>
2020-05-13 20:45:32 -0400

    promoted react-UX to web_app

--- WebApp/controllers/force.js
+++
@@ -1,9 +0,0 @@
-exports.force_mod = function(req, res, next) {
-  res.render('force_mod', { title: 'Bulletproof Asset Provenance' });
-}
-
-exports.submit_mod = function(req, res, next) {
-  console.log("Asset Id", req.body.assetId);
-  console.log("New Rights holder", req.body.new);
-  res.redirect('/');
-}
--- WebApp/controllers/retrieve.js
+++
@@ -1,8 +0,0 @@
-exports.retrieve_record = function(req, res, next) {
-  res.render('retrieve_record', { title: 'Bulletproof Asset Provenance' });
-}
-
-exports.submit_retrieve = function(req, res, next) {
-  console.log("Asset Id", req.body.assetId);
-  res.redirect('/');
-}
