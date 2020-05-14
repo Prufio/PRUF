233e44a375e3095d80f59575cd75f5b75ef9c134
James B Smyth <jamesblaydesmyth@gmail.com>
2020-05-14 13:48:41 -0400

    Adding functionality to forms in web_app

--- web_app/src/Main.js
+++ web_app/src/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import Home from "./Home";
 import AddNote from "./AddNote";
@@ -9,6 +9,43 @@ import ModifyRecordStatus from "./ModifyRecordStatus";
 import NewRecord from "./NewRecord";
 import RetrieveRecord from "./RetrieveRecord";
 import TransferAsset from "./TransferAsset";
+import returnAbi from './abi';
+import returnSAbi from './sAbi';
+import Web3 from 'web3';
+
+function App () {
+  let web3 = require('web3');
+  const keccak256 = require('js-sha3').keccak256;
+  const bulletproof_frontend_addr = "0xCc2CBfd27fbf7AEF15FFfe119B91c3006B5DE0b0";
+  const bulletproof_storage_addr = "0xec7C54c5A4F454fA951077A6D200A73910eB1ae0";
+  const ethereum = window.ethereum;
+
+  web3 = new Web3(web3.givenProvider);
+  const myAbi = returnAbi();
+  const sAbi = returnSAbi();
+  const bulletproof = new web3.eth.Contract(myAbi, bulletproof_frontend_addr);
+  const storage = new web3.eth.Contract(sAbi, bulletproof_storage_addr);
+
+  var [addr, setAddr] = useState('');
+
+  window.addEventListener('load', async () => {
+
+    await ethereum.enable();
+    web3.eth.getAccounts().then(e => setAddr(e[0]));
+    
+    if (web3.eth.getAccounts().then(e => e === addr)) {
+      console.log("Serving current metamask address at accounts[0]");
+
+    }
+
+    ethereum.on('accountsChanged', function (accounts) {
+      console.log('trying to change active address');
+      web3.eth.getAccounts().then(e => setAddr(e[0]));
+    })
+
+  })
+
+}
 
 class Main extends Component {
   render() {
@@ -73,3 +110,4 @@ class Main extends Component {
 }
 
 export default Main;
+export default App;
