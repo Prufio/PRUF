bdbdb6a889b065486535316e94515e8d930112fd
CT Smyth <cliff.smyth@gmail.com>
2020-05-20 21:02:11 -0400

    Implemented verifyRightsholder

--- web_app/src/RetrieveRecord.js
+++ web_app/src/RetrieveRecord.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Web3Listener from "./Web3Listener";
+
 function RetrieveRecord() {
   let web3 = Web3Listener("web3");
   let addr = Web3Listener("addr");
@@ -10,89 +11,89 @@ function RetrieveRecord() {
   var [model, setModel] = useState("");
   var [serial, setSerial] = useState("");
 
-
   var [result, setResult] = useState([]);
 
-  
   const _retrieveRecord = () => {
-
-    var idxHash = (web3.utils.soliditySha3(type, manufacturer, model, serial));
+    var idxHash = web3.utils.soliditySha3(type, manufacturer, model, serial);
 
     console.log("idxHash", idxHash);
 
     storage.methods
       .retrieveRecord(idxHash)
-      .call({ from: addr}).then(_result => {setResult(_result);});
+      .call({ from: addr })
+      .then((_result) => {
+        setResult(_result);
+      });
   };
 
   return (
     <div>
-    <form className="RRform">
-      <h2>Search for Record</h2>
-      Type:
-      <input
-        type="text"
-        name="type"
-        placeholder="Type"
-        required
-        onChange={(e) => setType(e.target.value)}
-      />
-      <br></br>
-      Manufacturer:
-      <input
-        type="text"
-        name="manufacturer"
-        placeholder="Manufacturer"
-        required
-        onChange={(e) => setManufacturer(e.target.value)}
-      />
-      <br></br>
-      Model:
-      <input
-        type="text"
-        name="model"
-        placeholder="Model"
-        required
-        onChange={(e) => setModel(e.target.value)} 
-      />
-      <br></br>
-      Serial:
-      <input
-        type="text"
-        name="serial"
-        placeholder="Serial Number"
-        required
-        onChange={(e) => setSerial(e.target.value)} 
-      />
-      <br></br>
-      <input type="button" value="Retrieve Record"  onClick={_retrieveRecord}/>
-    </form>
+      <form className="RRform">
+        <h2>Search for Record</h2>
+        Type:
+        <input
+          type="text"
+          name="type"
+          placeholder="Type"
+          required
+          onChange={(e) => setType(e.target.value)}
+        />
+        <br></br>
+        Manufacturer:
+        <input
+          type="text"
+          name="manufacturer"
+          placeholder="Manufacturer"
+          required
+          onChange={(e) => setManufacturer(e.target.value)}
+        />
+        <br></br>
+        Model:
+        <input
+          type="text"
+          name="model"
+          placeholder="Model"
+          required
+          onChange={(e) => setModel(e.target.value)}
+        />
+        <br></br>
+        Serial:
+        <input
+          type="text"
+          name="serial"
+          placeholder="Serial Number"
+          required
+          onChange={(e) => setSerial(e.target.value)}
+        />
+        <br></br>
+        <input
+          type="button"
+          value="Retrieve Record"
+          onClick={_retrieveRecord}
+        />
+      </form>
 
-    {(result[5] > 0) &&     //conditional rendering
-      <div className = "RRresults">
-        Status: 
-        {result[3]}
-        <br></br>
-        Mod Count: 
-        {result[4]}
-        <br></br>
-        Asset Class : 
-        {result[5]}
-        <br></br>
-        Count : 
-        {result[6]} of {result[7]}
-        <br></br>
-        <br></br>
-        Description Hash : 
-        <br></br>
-        {result[8]}
-        <br></br>
-        <br></br>
-        Note Hash : 
-        <br></br>
-        {result[9]}
-      </div>
-    }
+      {result[5] > 0 && ( //conditional rendering
+        <div className="RRresults">
+          Status:
+          {result[3]}
+          <br></br>
+          Mod Count:
+          {result[4]}
+          <br></br>
+          Asset Class :{result[5]}
+          <br></br>
+          Count :{result[6]} of {result[7]}
+          <br></br>
+          <br></br>
+          Description Hash :<br></br>
+          {result[8]}
+          <br></br>
+          <br></br>
+          Note Hash :<br></br>
+          {result[9]}
+        </div>
+      )}
     </div>
   );
 }
--- web_app/src/VerifyRightsholder.js
+++ web_app/src/VerifyRightsholder.js
@@ -2,60 +2,154 @@ import React, { useState } from "react";
 import Web3Listener from "./Web3Listener";
 
 function VerifyRightsholder() {
-  let addr = Web3Listener("addr");
-  let frontend = Web3Listener("frontend");
   let web3 = Web3Listener("web3");
+  let addr = Web3Listener("addr");
+  let storage = Web3Listener("storage");
 
-  var [idxHash, setidxHash] = useState("");
-  var [rgtHash, setrgtHash] = useState("");
-  var [newIpfs1, setNewIpfs1] = useState("");
   var [txHash, setTxHash] = useState("");
-  const _modifyDescription = () => {
-    console.log(   //------------------------------------------remove ------security
-      "Sending data: ",
-      idxHash,
-      rgtHash,
-      newIpfs1
-    );
+  var [txLink, setTxLink] = useState("");
 
-  let _rgtHash = (web3.utils.soliditySha3(idxHash, rgtHash));
-    console.log('NewHash', _rgtHash);
-    console.log('idxHash', idxHash);
+  var [type, setType] = useState("");
+  var [manufacturer, setManufacturer] = useState("");
+  var [model, setModel] = useState("");
+  var [serial, setSerial] = useState("");
 
-    frontend.methods
-      ._modIpfs1(idxHash, _rgtHash, newIpfs1)
-      .send({ from: addr})
+  var [first, setFirst] = useState("");
+  var [middle, setMiddle] = useState("");
+  var [surname, setSurname] = useState("");
+  var [id, setID] = useState("");
+  var [secret, setSecret] = useState("");
+
+  var [result, setResult] = useState("");
+
+  const _verify = () => {
+    var idxHash = web3.utils.soliditySha3(type, manufacturer, model, serial);
+    var rgtRaw = web3.utils.soliditySha3(first, middle, surname, id, secret);
+    var rgtHash = web3.utils.soliditySha3(idxHash, rgtRaw);
+
+    console.log("idxHash", idxHash);
+    console.log("New rgtRaw", rgtRaw);
+    console.log("New rgtHash", rgtHash);
+
+    storage.methods
+      .blockchainVerifyRightsHolder(idxHash, rgtHash)
+      .send({ from: addr, value: web3.utils.toWei("0.00") })
       .on("receipt", (receipt) => {
         setTxHash(receipt.transactionHash);
+       //Stuff to do here when tx confirmed?
       });
+
     console.log(txHash);
+
+    storage.methods
+      ._verifyRightsHolder(idxHash, rgtHash)
+      .call({ from: addr })
+      .then((_result) => {
+        setResult(_result);
+      });
   };
 
   return (
-    <form className="VRform" onSubmit={_modifyDescription}>
-      <h2>Verify</h2>
-      Asset ID:
+    <div>
+      <form className="VRform">
+        <h2>Verify Provenance</h2>
+        Type:
       <input
         type="text"
-        name="idxHashField"
-        placeholder="Asset ID"
+        name="type"
+        placeholder="Type"
         required
-        onChange={(e) => setidxHash(web3.utils.keccak256(e.target.value))}
+        onChange={(e) => setType(e.target.value)} 
       />
       <br></br>
-      Rights Holder:
+      Manufacturer:
       <input
         type="text"
-        name="rgtHashField"
-        placeholder="Rights Holder"
+        name="manufacturer"
+        placeholder="Manufacturer"
         required
-        onChange={(e) => setrgtHash(web3.utils.keccak256(e.target.value))}
+        onChange={(e) => setManufacturer(e.target.value)} 
       />
       <br></br>
-      <input type="radio" value="true" name="verify"/> on blockchain
+      Model:
+      <input
+        type="text"
+        name="model"
+        placeholder="Model"
+        required
+        onChange={(e) => setModel(e.target.value)} 
+      />
       <br></br>
-      <input type="submit" value="Verify" />
-    </form>
+      Serial:
+      <input
+        type="text"
+        name="serial"
+        placeholder="Serial Number"
+        required
+        onChange={(e) => setSerial(e.target.value)}
+      />
+      <br></br>
+      First Name:
+      <input
+        type="text"
+        name="first"
+        placeholder="First name"
+        required
+        onChange={(e) => setFirst(e.target.value)}
+      />
+      <br></br>
+      Middle Name:
+      <input
+        type="text"
+        name="middle"
+        placeholder="Middle name"
+        required
+        onChange={(e) => setMiddle(e.target.value)}
+      />
+      <br></br>
+      Surname:
+      <input
+        type="text"
+        name="surname"
+        placeholder="Surname"
+        required
+        onChange={(e) => setSurname(e.target.value)}
+      />
+      <br></br>
+      ID:
+      <input
+        type="text"
+        name="id"
+        placeholder="ID"
+        required
+        onChange={(e) => setID(e.target.value)}
+      />
+      <br></br>
+      Password:
+      <input
+        type="text"
+        name="secret"
+        placeholder="Secret"
+        required
+        onChange={(e) => setSecret(e.target.value)}
+      />
+        <br></br>
+        <input
+          type="button"
+          value="Verify"
+          onClick={_verify}
+        />
+      </form>
+
+      {txHash > 0 && ( //conditional rendering
+        <div className="VRresults">
+          {result === "170"? ('Match Confirmed') : ('No match found')}
+          <br></br>
+          <br></br>
+          <a href={"https://kovan.etherscan.io/tx/"+txHash}>Etherscan:{txHash}</a>
+        </div>
+      )}
+    </div>
   );
 }
 
