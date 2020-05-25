950b51302c8b094309e7b8d186e80a233f01f95c
Drake Smyth <drakesmyth@gmail.com>
2020-05-25 13:46:11 -0400

    Updated storage contract in web_app

--- web_app/src/Web3Listener.js
+++ web_app/src/Web3Listener.js
@@ -7,7 +7,7 @@ function Web3Listener(request) {
   const bulletproof_frontend_addr =
     "0x9Ef2BBF052A5b61eBD1452d48B515BE7659a200B";
   const bulletproof_storage_addr =
-   "0x9b68c42E981FCA6832098bBC9953abA74F66FD26";
+   "0xa031B96b9d05B05034875612c0436a0e4d88f41e";
 
   let web3 = require("web3");
   const ethereum = window.ethereum;
