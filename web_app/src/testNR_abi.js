3d69612f753272f562880179f643f691117031db
CT Smyth <cliff.smyth@gmail.com>
2020-05-25 14:35:47 -0400

    Edited testNR ABI

--- web_app/src/testNR_abi.js
+++ web_app/src/testNR_abi.js
@@ -1,5 +1,99 @@
 function returnTestNRAbi() {
-	return ();
+	return ([
+		{
+			"inputs": [
+				{
+					"internalType": "bytes32",
+					"name": "_userHash",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "_idxHash",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "_rgt",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "uint16",
+					"name": "_assetClass",
+					"type": "uint16"
+				},
+				{
+					"internalType": "uint256",
+					"name": "_countDownStart",
+					"type": "uint256"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "_Ipfs1",
+					"type": "bytes32"
+				}
+			],
+			"name": "newRecord",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"internalType": "string",
+					"name": "_msg",
+					"type": "string"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "_tokenID",
+					"type": "uint256"
+				},
+				{
+					"indexed": false,
+					"internalType": "bytes32",
+					"name": "_userHash",
+					"type": "bytes32"
+				},
+				{
+					"indexed": false,
+					"internalType": "bytes32",
+					"name": "_idxHash",
+					"type": "bytes32"
+				},
+				{
+					"indexed": false,
+					"internalType": "bytes32",
+					"name": "_rgt",
+					"type": "bytes32"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint16",
+					"name": "_assetClass",
+					"type": "uint16"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "_countDownStart",
+					"type": "uint256"
+				},
+				{
+					"indexed": false,
+					"internalType": "bytes32",
+					"name": "_Ipfs1",
+					"type": "bytes32"
+				}
+			],
+			"name": "REPORT",
+			"type": "event"
+		}
+	]);
 }
 
 export default returnTestNRAbi; 
