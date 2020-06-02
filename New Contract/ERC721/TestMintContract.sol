09b4b5ad913f14734887350b7605c87f1db18fa7
Drake Smyth <drakesmyth@gmail.com>
2020-06-02 15:51:19 -0400

    ...

--- New Contract/ERC721/TestMintContract.sol
+++ New Contract/ERC721/TestMintContract.sol
@@ -4,20 +4,11 @@ pragma solidity ^0.6.0;
 
 import "./ERC721.sol";
 import "./Ownable.sol";
-
+import "./IERC721Receiver.sol";
 
 contract BulletProofMinter is ERC721, Ownable {
 
 constructor() ERC721("BulletproofToken", "BPT") public { }
-
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external virtual returns (bytes4) {
-
-    }
 
     function mint(address to, uint256 tokenId) public virtual {
         _safeMint(to, tokenId, "");
