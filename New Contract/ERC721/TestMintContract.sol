ccc70aaba311ccca1d16fc098d4a87f455e87844
Drake Smyth <drakesmyth@gmail.com>
2020-06-02 12:34:39 -0400

    ...

--- New Contract/ERC721/TestMintContract.sol
+++ New Contract/ERC721/TestMintContract.sol
@@ -1,33 +1,33 @@
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
-import "./Context.sol";
-import "./IERC721.sol";
-import "./IERC721Metadata.sol";
-import "./IERC721Receiver.sol";
-import "./ERC165.sol";
-import "./SafeMath.sol";
-import "./Address.sol";
-import "./Strings.sol";
 import "./ERC721.sol";
 import "./Ownable.sol";
 
-/**
- * @title ERC721 Non-Fungible Token Standard basic implementation
- * @dev see https://eips.ethereum.org/EIPS/eip-721
- */
-contract BulletProofMinter is ERC721, Ownable {
-    using SafeMath for uint256;
-    using Address for address;
-    using Strings for uint256;
 
-   
-    function mint(address to, uint256 tokenId) internal virtual {
+contract BulletProofMinter is ERC721, Ownable {
+
+constructor() ERC721("BulletproofToken", "BPT") public { }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external virtual returns (bytes4) {
+
+    }
+
+    function mint(address to, uint256 tokenId) public virtual {
         _safeMint(to, tokenId, "");
     }
-   
-    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
+
+    function transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {
         safeTransferFrom(from, to, tokenId, "");
     }
-
 }
