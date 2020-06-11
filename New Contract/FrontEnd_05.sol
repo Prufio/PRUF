cfc541e5ebf894d30af0775d9288c2b4ade39bbb
CT Smyth <cliff.smyth@gmail.com>
2020-06-11 13:15:09 -0400

    minor

--- New Contract/FrontEnd_05.sol
+++ New Contract/FrontEnd_05.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.2;
-
 import "./PullPayment.sol";
 
-    /*
-     * @dev Set wallet for contract to direct payments to
-     */
+/*--------To do
+*Status 5  - Asset transferred - implies that asset holder is the owner.
+*       must be re-imported by ACadmin through regular onboarding process
+*       no actions besides modify RGT to a new rightsholder can be performed on a statuss 5 asset (no status changes) (Frontend)
+
+*=Status 0 Default asset creation status, 
+default after FMR, and after status 5 (essentially a FMR) (IN frontend) 
+// only type 1 can change a status 0 record
+*
+*Status 1-5 No actions can be performed by type 9 users. (real ACAdmins only can set or unset these statuses) except:
+*Automation can change a 1 or 2 status to any automated status
+*
+*status 6 transferrable, automation set/unset (secret confirmed)(ACAdmin can unset)
+*status 7 non-transferrable, automation set/unset (secret confirmed)(ACAdmin can unset)
+*status 8 stolen (automation set)(ONLY ACAdmin can unset)
+*status 9 lost (automation set/unset)(ACAdmin can unset)
+*
+*/
+
 
 
 interface StorageInterface {
@@ -176,6 +191,7 @@ contract FrontEnd is PullPayment, Ownable {
             rec.forceModCount++;
         }
 
+        rec.assetStatus = 0;
         rec.rightsHolder = _rgtHash;
 
         writeRecord(_idxHash, rec);
