760bd57f90f3b3a19614dcdc3b18e5bb945a6179
Drake Smyth <drakesmyth@gmail.com>
2020-06-22 10:48:01 -0400

    Updated contract version in web_app

--- web_app_new/src/Contracts.js
+++ web_app_new/src/Contracts.js
@@ -1,7 +1,7 @@
 function returnAddresses() {
   var addrArray = [];
-  const storageAddress = "0x9f6bdb8269c5512757a00D6a65684E248ca594C1";
-  const frontEndAddress = "0x4448Cc0aFD9C416E2b74f665aF4b8D28a98949Cf";
+  const storageAddress = "0xCd3dEfdD0d0684BEB6a2F44d6da6Af4b07D49A28";
+  const frontEndAddress = "0x5A89a29Fb0a10DDeB765fD16c87a8D015433D86F";
   addrArray[0] = storageAddress;
   addrArray[1] = frontEndAddress;
   return addrArray;
