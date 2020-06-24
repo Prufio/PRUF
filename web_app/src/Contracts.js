b56c18fb17f69c18f0aee5d4144546efb7c9a060
James B Smyth <jamesblaydesmyth@gmail.com>
2020-06-24 16:24:49 -0400

    Adding split FE contract params

--- web_app/src/Contracts.js
+++ web_app/src/Contracts.js
@@ -1,9 +1,13 @@
 function returnAddresses() {
   var addrArray = [];
   const storageAddress = "0x9c5910f6B29d3563217395D7259875eF8E540Ef0";
-  const frontEndAddress = "0x2dA766ACD51b0a420713764B3CB4c632c609eB63";
+  const BPFreeAddress = "0x2dA766ACD51b0a420713764B3CB4c632c609eB63";
+  const BPPayableAddress = "";
+  
   addrArray[0] = storageAddress;
-  addrArray[1] = frontEndAddress;
+  addrArray[1] = BPFreeAddress;
+  addrArray[2] = BPPayableAddress;
+  
   return addrArray;
 }
 
