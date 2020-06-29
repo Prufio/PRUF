2f84f2f021a7a8247796fbca2359b5f6c6b7f258
James B Smyth <jamesblaydesmyth@gmail.com>
2020-06-29 10:06:55 -0400

    ...

--- web_app/src/Ownership.js
+++ web_app/src/Ownership.js
@@ -40,6 +40,7 @@ class Ownership extends Component {
       toggle: false,
       assetClass: "",
       storage: "",
+      web3: null,
     };
   }
 
@@ -48,7 +49,6 @@ class Ownership extends Component {
     _web3 = new Web3(_web3.givenProvider);
     this.setState({ web3: _web3 });
     _web3.eth.getAccounts().then((e) => this.setState({ addr: e[0] }));
-    this.returnsContract();
     document.addEventListener("accountListener", this.acctChanger());
   }
 
