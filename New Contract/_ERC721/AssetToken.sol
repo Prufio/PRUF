0ad89ae478e491a78cd189871a3c9ea9d42d796a
CT Smyth <cliff.smyth@gmail.com>
2020-06-29 10:02:14 -0400

    Minor changes to AssetToken

--- New Contract/_ERC721/AssetToken.sol
+++ New Contract/_ERC721/AssetToken.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.7;
 
 import "./ERC721.sol";
 import "./Ownable.sol";
-
 
 contract AssetToken is ERC721, Ownable {
     mapping(bytes32 => uint8) private registeredAdmins; // Authorized recorder database
@@ -14,6 +13,7 @@ contract AssetToken is ERC721, Ownable {
     event REPORT(string _msg);
 
     modifier isAdmin() {
+
         require(
             (registeredAdmins[keccak256(abi.encodePacked(msg.sender))] == 1) ||
                 (owner() == msg.sender),
@@ -24,48 +24,53 @@ contract AssetToken is ERC721, Ownable {
 
     function mintAssetToken(
         address _reciepientAddress,
-        bytes32 _idxHash,
+        uint256 tokenId,
         string calldata _tokenURI
     ) external isAdmin returns (uint256) {
-        uint256 tokenId = uint256(_idxHash);
+        //^^^^^^^checks^^^^^^^^^
         _safeMint(_reciepientAddress, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-
         return tokenId;
+        //^^^^^^^interactions^^^^^^^^^
     }
 
     function transferAssetToken(
-        address from,
+         address from,
         address to,
-        bytes32 _idxHash
+        uint256 tokenId
     ) external onlyOwner {
-        uint256 tokenId = uint256(_idxHash);
+        //^^^^^^^checks^^^^^^^^^
         safeTransferFrom(from, to, tokenId);
+        //^^^^^^^interactions^^^^^^^^^
     }
 
     function reMintAssetToken(
-        address _reciepientAddress,
-        bytes32 _idxHash,
+         address _reciepientAddress,
+        uint256 tokenId,
         string calldata _tokenURI
     ) external isAdmin returns (uint256) {
-        uint256 tokenId = uint256(_idxHash);
         require(_exists(tokenId), "Cannot Remint nonexistant token");
+        //^^^^^^^checks^^^^^^^^^
         _burn(tokenId);
         _safeMint(_reciepientAddress, tokenId);
         _setTokenURI(tokenId, _tokenURI);
-
         return tokenId;
+        //^^^^^^^interactions^^^^^^^^^
     }
 
-    function OO_addAdmin(
+    function OO_addAssetTokenAdmin(
         address _authAddr,
         uint8 _addAdmin // must make this indelible / permenant???????? SECURITY / trustless goals
     ) external onlyOwner {
         bytes32 addrHash = keccak256(abi.encodePacked(_authAddr));
-
-        require(_addAdmin == 1, "Admin not added");
-
+        require(
+            (_addAdmin == 1) || (_addAdmin == 0),
+            "Admin status must be 1 or 0"
+        );
+        //^^^^^^^checks^^^^^^^^^
         registeredAdmins[addrHash] = _addAdmin;
+        //^^^^^^^effects^^^^^^^^^
         emit REPORT("internal user database access!"); //report access to the internal user database
+        //^^^^^^^interactions^^^^^^^^^
     }
 }
