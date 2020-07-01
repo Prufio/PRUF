586e5b754deddd0c393223757cd4746aa0ba80a5
CT Smyth <cliff.smyth@gmail.com>
2020-07-01 11:40:50 -0400

    Bug in pricing

--- New Contract/PRUF_storage_063.sol
+++ New Contract/PRUF_storage_063.sol
@@ -747,8 +747,6 @@ contract Storage is Ownable, ReentrancyGuard {
         );
         //^^^^^^^checks^^^^^^^^
 
-        returnCosts = baseCost;
-
             // returnCosts.cost1 = costs.cost1.add(baseCost.cost1);
             // returnCosts.cost2 = costs.cost2.add(baseCost.cost2);
             // returnCosts.cost3 = costs.cost3.add(baseCost.cost3);
@@ -756,41 +754,41 @@ contract Storage is Ownable, ReentrancyGuard {
             // returnCosts.cost5 = costs.cost5.add(baseCost.cost5);
             // returnCosts.cost6 = costs.cost6.add(baseCost.cost6);
 
-        // if (costs.cost1 <= priceThreshold) {
-        //     returnCosts.cost1 = costs.cost1.add(baseCost.cost1);
-        // } else {
-        //     returnCosts.cost1 = costs.cost1.add(costs.cost1.div(priceDivisor));
-        // }
+        if (costs.cost1 <= priceThreshold) {
+            returnCosts.cost1 = costs.cost1.add(baseCost.cost1);
+        } else {
+            returnCosts.cost1 = costs.cost1.add(costs.cost1.div(priceDivisor));
+        }
 
-        // if (costs.cost2 <= priceThreshold) {
-        //     returnCosts.cost2 = costs.cost2.add(baseCost.cost2);
-        // } else {
-        //     returnCosts.cost2 = costs.cost2.add(costs.cost2.div(priceDivisor));
-        // }
+        if (costs.cost2 <= priceThreshold) {
+            returnCosts.cost2 = costs.cost2.add(baseCost.cost2);
+        } else {
+            returnCosts.cost2 = costs.cost2.add(costs.cost2.div(priceDivisor));
+        }
 
-        // if (costs.cost3 <= priceThreshold) {
-        //     returnCosts.cost3 = costs.cost3.add(baseCost.cost3);
-        // } else {
-        //     returnCosts.cost3 = costs.cost3.add(costs.cost3.div(priceDivisor));
-        // }
+        if (costs.cost3 <= priceThreshold) {
+            returnCosts.cost3 = costs.cost3.add(baseCost.cost3);
+        } else {
+            returnCosts.cost3 = costs.cost3.add(costs.cost3.div(priceDivisor));
+        }
 
-        // if (costs.cost4 <= priceThreshold) {
-        //     returnCosts.cost4 = costs.cost4.add(baseCost.cost4);
-        // } else {
-        //     returnCosts.cost4 = costs.cost4.add(costs.cost4.div(priceDivisor));
-        // }
+        if (costs.cost4 <= priceThreshold) {
+            returnCosts.cost4 = costs.cost4.add(baseCost.cost4);
+        } else {
+            returnCosts.cost4 = costs.cost4.add(costs.cost4.div(priceDivisor));
+        }
 
-        // if (costs.cost5 <= priceThreshold) {
-        //     returnCosts.cost5 = costs.cost5.add(baseCost.cost5);
-        // } else {
-        //     returnCosts.cost5 = costs.cost5.add(costs.cost5.div(priceDivisor));
-        // }
+        if (costs.cost5 <= priceThreshold) {
+            returnCosts.cost5 = costs.cost5.add(baseCost.cost5);
+        } else {
+            returnCosts.cost5 = costs.cost5.add(costs.cost5.div(priceDivisor));
+        }
 
-        // if (costs.cost6 <= priceThreshold) {
-        //     returnCosts.cost6 = costs.cost6.add(baseCost.cost6);
-        // } else {
-        //     returnCosts.cost6 = costs.cost6.add(costs.cost6.div(priceDivisor));
-        // }
+        if (costs.cost6 <= priceThreshold) {
+            returnCosts.cost6 = costs.cost6.add(baseCost.cost6);
+        } else {
+            returnCosts.cost6 = costs.cost6.add(costs.cost6.div(priceDivisor));
+        }
         //^^^^^^^effects^^^^^^^^^
 
         return (
