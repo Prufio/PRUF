ddd4a8b36de92d367cb8e5a47695c6c6638d49e7
CT Smyth <cliff.smyth@gmail.com>
2020-07-07 13:55:30 -0400

    File organization

---
+++ Depricated/063/PRUF_ACToken.sol
@@ -0,0 +1,196 @@
+//TODO: Recycle
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.7;
+
+import "./_ERC721/ERC721.sol";
+import "./_ERC721/Ownable.sol";
+import "./PRUF_interfaces.sol";
+import "./Imports/ReentrancyGuard.sol";
+
+contract AssetClassToken is Ownable, ReentrancyGuard, ERC721 {
+
+    constructor() public ERC721("PRüF Asset Class Token", "PAC") {}
+
+    address internal ACmanagerAddress; //isAdmin
+    address internal storageAddress;
+    StorageInterface internal Storage; // Set up external contract interface
+
+    event REPORT(string _msg);
+
+    modifier isAdmin() {
+        require(
+            (msg.sender == ACmanagerAddress) ||
+                (msg.sender == owner()),
+            "Calling address does not belong to an Admin"
+        );
+        _;
+    }
+
+    //----------------------Internal Admin functions / onlyowner or isAdmin----------------------//
+
+    /*
+     * @dev Set storage contract to interface with
+     */
+    function OO_setStorageContract(address _storageAddress) external onlyOwner {
+        require(
+            _storageAddress != address(0),
+            "PC:SSC: storage address cannot be zero"
+        );
+        //^^^^^^^checks^^^^^^^^^
+
+        Storage = StorageInterface(_storageAddress);
+        //^^^^^^^effects^^^^^^^^^
+    }
+
+    /*
+     * @dev Address Setters
+     */
+    function OO_ResolveContractAddresses() external nonReentrant onlyOwner {
+        //^^^^^^^checks^^^^^^^^^
+        ACmanagerAddress = Storage.resolveContractAddress("ACmanager");
+        //^^^^^^^effects^^^^^^^^^
+    }
+
+    /*
+     * must be isAdmin
+     *
+     */
+    function mintACToken(
+        address _recipientAddress,
+        uint256 tokenId,
+        string calldata _tokenURI
+    ) external isAdmin returns (uint256) {
+        //^^^^^^^checks^^^^^^^^^
+        _safeMint(_recipientAddress, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        return tokenId;
+        //^^^^^^^interactions^^^^^^^^^
+    }
+
+     /*
+     * Authorizations?
+     * @dev remint Asset Token
+     * must set a new and unuiqe rgtHash
+     * burns old token
+     * Sends new token to original Caller
+     */
+    function reMintACToken(
+        address _recipientAddress,
+        uint256 tokenId,
+        string calldata _tokenURI
+    ) external isAdmin returns (uint256) {
+        require(_exists(tokenId), "Cannot Remint nonexistant token");
+        //^^^^^^^checks^^^^^^^^^
+        _burn(tokenId);
+        _safeMint(_recipientAddress, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        return tokenId;
+        //^^^^^^^interactions^^^^^^^^^
+    }
+
+    /**
+     * @dev Transfers the ownership of a given token ID to another address.
+     * Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
+     * Requires the msg.sender to be the owner, approved, or operator.
+     * @param from current owner of the token
+     * @param to address to receive the ownership of the given token ID
+     * @param tokenId uint256 ID of the token to be transferred
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override nonReentrant {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        //^^^^^^^checks^^^^^^^^^
+
+        _transfer(from, to, tokenId);
+        //^^^^^^^interactions^^^^^^^^^
+    }
+
+    /**
+     * @dev Safely transfers the ownership of a given token ID to another address
+     * If the target address is a contract, it must implement {IERC721Receiver-onERC721Received},
+     * which is called upon a safe transfer, and return the magic value
+     * `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`; otherwise,
+     * the transfer is reverted.
+     * Requires the msg.sender to be the owner, approved, or operator
+     * @param from current owner of the token
+     * @param to address to receive the ownership of the given token ID
+     * @param tokenId uint256 ID of the token to be transferred
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override nonReentrant {
+        //^^^^^^^checks^^^^^^^^^
+
+        safeTransferFrom(from, to, tokenId, "");
+        //^^^^^^^interactions^^^^^^^^^
+    }
+
+    /**
+     * @dev Safely transfers the ownership of a given token ID to another address
+     * If the target address is a contract, it must implement {IERC721Receiver-onERC721Received},
+     * which is called upon a safe transfer, and return the magic value
+     * `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`; otherwise,
+     * the transfer is reverted.
+     * Requires the _msgSender() to be the owner, approved, or operator
+     * @param from current owner of the token
+     * @param to address to receive the ownership of the given token ID
+     * @param tokenId uint256 ID of the token to be transferred
+     * @param _data bytes data to send along with a safe transfer check
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public virtual override nonReentrant {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        //^^^^^^^checks^^^^^^^^^
+        _safeTransfer(from, to, tokenId, _data);
+        //^^^^^^^interactions^^^^^^^^^
+    }
+
+    // /**
+    //  * @dev Safely burns a token and sets the corresponding RGT to zero in storage.
+    //  */
+    // function burn(uint256 tokenId) external nonReentrant {
+    //     bytes32 _idxHash = bytes32(tokenId);
+    //     Record memory rec = getRecord(_idxHash);
+
+    //     require(
+    //         (rec.assetStatus != 3) ||
+    //             (rec.assetStatus != 4) ||
+    //             (rec.assetStatus != 6) ||
+    //             (rec.assetStatus != 50) ||
+    //             (rec.assetStatus != 53) ||
+    //             (rec.assetStatus != 54) ||
+    //             (rec.assetStatus != 56),
+    //         "Asset in lost, stolen, or escrow status"
+    //     );
+
+    //     require(
+    //         _isApprovedOrOwner(_msgSender(), tokenId),
+    //         "ERC721: transfer caller is not owner nor approved"
+    //     );
+    //     //^^^^^^^checks^^^^^^^^^
+    //     rec.rightsHolder = 0x0; //burn in storage
+    //     //^^^^^^^effects^^^^^^^^^
+    //     writeRecord(_idxHash, rec);
+    //     _burn(tokenId);
+    //     //^^^^^^^interactions^^^^^^^^^
+    // }
+
+
+}
