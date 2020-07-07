7091befb7525730e3785543fb6d7c3c905844d6d
CT Smyth <cliff.smyth@gmail.com>
2020-07-07 14:32:35 -0400

    Minor version edits

--- New Contract/PRUF_NP_064.sol
+++ New Contract/PRUF_NP_064.sol
@@ -82,7 +82,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.7;
 
-import "./PRUF_core_063.sol";
+import "./PRUF_core_064.sol";
 
 contract PRUF_NP is PRUF {
     using SafeMath for uint256;
