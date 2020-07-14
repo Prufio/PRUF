1897b341107aee83ec97959f1d25b7f9925111ab
CT Smyth <cliff.smyth@gmail.com>
2020-07-14 14:57:55 -0400

    TO DO docs

--- New Contract/_PRUF_DOCS.sol
+++ New Contract/_PRUF_DOCS.sol
@@ -1,13 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.9;
 
 /*-----------------------------------------------------------------
  *  TO DO :
+ *  AssetTokenContract.burn still just burns, sets 0x0 and 60)  is this wanted?
  *  
- *  Implement recycle "Burn" SAME THING AS RECYCLE?
- *  Implement recycle "Recycle"
- *          (assetTokenContract.burn still just burns, sets 0x0 and 60)
+ *  Implement Recycle   
+ *          Not possible in Custodial asset classes
+ *          Caller must hold token, must be in status 59
+ *          Token goes into "indefinite" escrow, RGT set to 0xFFF...
+ *              Caller is escrow controller, but escrow contract is "owner", can break escrow (resets to status 51)
+ *              Price set when escrow is to be broken by reregistering, from costs of the category 
+ *                     that it is to be imported into (endEscrow called from T_PRUF_APP?)
+ *              Importing breaks the escrow
+ *                  payment divided between ACroot, ACholder, and recycling address (escrow owner)     
+ *                  token sent to new owner (payment sender), status set to 51
+ *
+ *
+ *
+ *  Implement export
+ *            FROM Custodial:
+ *                 sets to status 51
+ *                 sends token to rightsholder wallet
+ *                 sets asset to status 70
+ *            FROM NonCustodial
+ *                 sets asset status to 70
+ *  Implement Import
+ *            TO Custodial
+ *                 Holder sends to custudial contract
+ *                 Authorized agent approves / rejects import
+ *                          change asset class
+ *                     or to refuse, import then reexport
+ *            TO NonCustodial
+ *                 Import checks for token posession
+ *                          change asset class
+ *  
+ *  ** Make a function in storage that changes the asset class
+ *                 
+ *  ** Status 70:
+ *          is a semi-transferrable status
+ *          limit sending to approved addresses
+ *                  Implement a separate status 70 safeTransfer function                             
+*-----------------------------------------------------------------
+ * FUTURE FEATURES
+ *      "bottle deposit" on asset creation to encourage recycling
+ *  New Recycle :   
+ *          Not possible in Custodial asset classes
+ *          Caller must hold token, must be in status 59
+ *          Caller recieves deposit amount (how the bloody hell do we manage this????)
+ *          Token goes into "indefinite" escrow, RGT set to 0xFFF...
+ *              Caller is escrow controller, but escrow contract is "owner", can break escrow 
+ *                      (requires repayment of deposit amount, resets to status 51)
+ *              Price set when escrow is to be broken by reregistering, from costs of the category 
+ *                     that it is to be imported into (endEscrow called from T_PRUF_APP?)
+ *              Importing breaks the escrow
+ *                  payment divided between ACroot, ACholder, and recycling address (escrow owner)     
+ *                  token sent to new owner (payment sender), status set to 51
+ *
+ *
+ *
+*-----------------------------------------------------------------
+
 *-----------------------------------------------------------------
 
 *-----------------------------------------------------------------
