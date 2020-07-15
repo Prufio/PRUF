d7e13e8fe39cee66f8f02ff32f3cbade7fd7e50d
Drake Smyth <drakesmyth@gmail.com>
2020-07-15 11:49:16 -0400

    truffle project setup

--- PRUF_TRUFFLE/migrations/1_initial_migration.js
+++ PRUF_TRUFFLE/migrations/1_initial_migration.js
@@ -1,8 +1,8 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
+  deployer.deploy(AssetClassToken);
   deployer.deploy(PRUF_AC_manager_065);
-  deployer.deploy(PRUF_ACToken);
   deployer.deploy(PRUF_app_065);
   deployer.deploy(PRUF_AssetToken);
   deployer.deploy(PRUF_NP_065);
