23db7a7db99ffe372c003402c482ee9864f450c7
Drake Smyth <drakesmyth@gmail.com>
2020-07-15 11:21:03 -0400

    truffle setup

--- PRUF_TRUFFLE/truffle-config.js
+++ PRUF_TRUFFLE/truffle-config.js
@@ -34,6 +34,12 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+    /* ... rest of truffle-config */
+    plugins: [
+      'truffle-plugin-solhint'
+    ],
+
+  
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -85,15 +91,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.6.9",    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
     }
   }
 }
