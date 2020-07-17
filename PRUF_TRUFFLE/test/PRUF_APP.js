e4c274f4d5902f36237b398b34af7d8cf9025a2d
Drake Smyth <drakesmyth@gmail.com>
2020-07-17 14:56:41 -0400

    Truffle testing

---
+++ PRUF_TRUFFLE/test/PRUF_APP.js
@@ -0,0 +1,19 @@
+const PRUF_APP = artifacts.require('PRUF_APP');
+
+
+contract('PRUF_APP', () => {
+    it('Should deploy Pruf_app properly', async () => {
+        const PRUF_app_065 = await PRUF_APP.deployed();
+        console.log(PRUF_app_065.address);
+        assert(PRUF_app_065.address !== '');
+    });
+    it("should add a record to storage", async () => {
+        let _idxHash = "0x3531cc3dc5bb231b65d260771886cc583d8fe8fb29b457554cb1930a722a747d";
+        let _rgtHash = "0xb083f25ffc9716fa6c018e077f602f3c6d2377f0bd01917fa75c4e9ca07bee6f";
+        let _assetClass = "10";
+        let hash = instance.addExam().call(hash_test, {from: accounts[0]});
+        assert.equal(hash.valueOf(), hash_test, "Not returning the correct address")
+    })
+
+});
+
--- PRUF_TRUFFLE/test/PRUF_storage.js
+++ PRUF_TRUFFLE/test/PRUF_storage.js
@@ -1,9 +1,12 @@
 const Storage = artifacts.require('Storage');
 
 contract('Storage', () => {
-    it('Should deploy smart contract properly', async () => {
+    it('Should deploy Storage properly', async () => {
         const PRUF_storage_065 = await Storage.deployed();
         console.log(PRUF_storage_065.address);
         assert(PRUF_storage_065.address !== '');
+
+        
     });
+
 });
