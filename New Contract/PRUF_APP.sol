1589a52a24d18e09dfa828904379e588137f8dfc
CT Smyth <cliff.smyth@gmail.com>
2020-08-05 11:36:05 -0400

    Changed export to txfr before ststus change

--- New Contract/PRUF_APP.sol
+++ New Contract/PRUF_APP.sol
@@ -113,10 +113,11 @@ contract APP is CORE {
         }
         rec.assetStatus = 70; // Set status to 70 (exported)
         //^^^^^^^effects^^^^^^^^^
-
+        
+        A_TKN.safeTransferFrom(address(this), _addr, tokenId); // sends token to rightsholder wallet (specified by auth user)
         writeRecord(_idxHash, rec);
         STOR.changeAC(_idxHash, AC_info.assetClassRoot);
-        A_TKN.safeTransferFrom(address(this), _addr, tokenId); // sends token to rightsholder wallet (specified by auth user)
+        
 
         return rec.assetStatus;
         //^^^^^^^interactions^^^^^^^^^
