e4be0b7e9a5a50a991bdb9d48fb9be915f64d7d1
Cliff <cliff.smyth@gmail.com>
2020-08-26 13:49:46 -0400

    Made interfaces self documenting

--- PRUF_TRUFFLE/contracts/PRUF_INTERFACES.sol
+++ PRUF_TRUFFLE/contracts/PRUF_INTERFACES.sol
@@ -22,109 +22,117 @@ interface AC_MGR_Interface {
     function getUserType(bytes32 _userHash, uint32 _assetClass)
         external
         view
-        returns (uint8);
+        returns (uint8 userTypeInAssetClass);
 
     function getAC_data(uint32 _assetClass)
         external
         returns (
-            uint32,
-            uint8,
-            uint32
+            uint32 assetClassRoot,
+            uint8 custodyType,
+            uint32 extendedData
         );
 
     function isSameRootAC(uint32 _assetClass1, uint32 _assetClass2)
         external
-        returns (uint8);
+        returns (uint8 uint8_Bool_type_0_170);
 
-    function getAC_name(uint32 _tokenId) external view returns (string memory);
+    function getAC_name(uint32 _tokenId)
+        external
+        view
+        returns (string memory ACname);
 
-    function resolveAssetClass(string calldata _name) external returns (uint32);
+    function resolveAssetClass(string calldata _name)
+        external
+        returns (uint32 assetClass);
 
     function ContractAC_auth(uint32 _assetClass, bytes32 _authContractNameHash)
         external
-        returns (uint8);
+        returns (uint8 contractTypeInAssetClass);
 
     function retrieveCosts(uint32 _assetClass)
         external
         returns (
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            address
+            uint256 newRecordCost,
+            uint256 transferAssetCost,
+            uint256 createNoteCost,
+            uint256 reMintRecordCost,
+            uint256 changeStatusCost,
+            uint256 forceModifyCost,
+            address paymentAddress
         );
 
     function retrieveBaseCosts()
         external
         returns (
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            address
+            uint256 newRecordCost,
+            uint256 transferAssetCost,
+            uint256 createNoteCost,
+            uint256 reMintRecordCost,
+            uint256 changeStatusCost,
+            uint256 forceModifyCost,
+            address paymentAddress
         );
 
     function getNewRecordCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 
     function getTransferAssetCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 
     function getCreateNoteCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 
     function getReMintRecordCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 
     function getChangeStatusCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 
     function getForceModifyCosts(uint32 _assetClass)
         external
         returns (
-            address,
-            uint256,
-            address,
-            uint256
+            address rootPaymentAddress,
+            uint256 rootFunctionCost,
+            address paymentAddress,
+            uint256 functionCost
         );
 }
 
 interface AC_TKN_Interface {
-    function ownerOf(uint256) external view returns (address);
+    function ownerOf(uint256 tokenId)
+        external
+        view
+        returns (address tokenHolderAdress);
 
     function safeTransferFrom(
         address from,
@@ -134,27 +142,32 @@ interface AC_TKN_Interface {
 
     function mintACToken(
         address _recipientAddress,
-        uint256 tokenId,
+        uint256 _tokenId,
         string calldata _tokenURI
-    ) external returns (uint256);
+    ) external returns (uint256 tokenId);
 
     function reMintACToken(
         address _recipientAddress,
-        uint256 tokenId,
+        uint256 _tokenId,
         string calldata _tokenURI
-    ) external returns (uint256);
+    ) external returns (uint256 tokenId);
 
-    function name() external view returns (string memory);
+    function name() external view returns (string memory tokenName);
 
-    function symbol() external view returns (string memory);
+    function symbol() external view returns (string memory tokenSymbol);
 
-    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function tokenURI(uint256 tokenId)
+        external
+        view
+        returns (string memory URI);
 }
 
 interface A_TKN_Interface {
-    function ownerOf(uint256) external returns (address);
+    function ownerOf(uint256 tokenId)
+        external
+        returns (address tokenHolderAdress);
 
-    function discard(uint256) external;
+    function discard(uint256 tokenId) external;
 
     function safeTransferFrom(
         address from,
@@ -164,27 +177,23 @@ interface A_TKN_Interface {
 
     function mintAssetToken(
         address _recipientAddress,
-        uint256 tokenId,
+        uint256 _tokenId,
         string calldata _tokenURI
-    ) external returns (uint256);
+    ) external returns (uint256 tokenId);
 
-    function reMintAssetToken(address _recipientAddress, uint256 tokenId)
+    function reMintAssetToken(address _recipientAddress, uint256 _tokenId)
         external
-        returns (uint256);
+        returns (uint256 tokenId);
 
-    function tokenExists(uint256 tokenId) external returns (uint8);
+    function tokenExists(uint256 tokenId)
+        external
+        returns (uint8 uint8_Bool_type_0_170);
 
-    function name() external view returns (string memory);
+    function name() external view returns (string memory tokenName);
 
-    function symbol() external view returns (string memory);
+    function symbol() external view returns (string memory tokenSymbol);
 
     function setURI(uint256 tokenId, string memory _tokenURI) external;
-
-    function validateNakedToken(
-        uint256 tokenId,
-        uint32 _assetClass,
-        string calldata _authCode
-    ) external;
 }
 
 interface STOR_Interface {
@@ -206,10 +215,7 @@ interface STOR_Interface {
 
     function changeAC(bytes32 _idxHash, uint32 _newAssetClass) external;
 
-    function setEscrow(
-        bytes32 _idxHash,
-        uint8 _newAssetStatus
-    ) external;
+    function setEscrow(bytes32 _idxHash, uint8 _newAssetStatus) external;
 
     function endEscrow(bytes32 _idxHash) external;
 
@@ -222,37 +228,22 @@ interface STOR_Interface {
     function retrieveRecord(bytes32 _idxHash)
         external
         returns (
-            bytes32,
-            uint8,
-            uint32,
-            uint32,
-            uint32,
-            bytes32,
-            bytes32
+            bytes32 rightsHolder,
+            uint8 assetStatus,
+            uint32 assetClass,
+            uint32 countDown,
+            uint32 countDownStart,
+            bytes32 Ipfs1,
+            bytes32 Ipfs2
         );
-
-    // function retrieveShortRecord(
-    //     bytes32 _idxHash //all but rgtHash
-    // )
-    //     external
-    //     returns (
-    //         uint8,
-    //         uint8,
-    //         uint256,
-    //         uint256,
-    //         uint256,
-    //         bytes32,
-    //         bytes32,
-    //         uint16
-    //     );
 
     function resolveContractAddress(string calldata _name)
         external
-        returns (address);
+        returns (address addressOfNamedContract);
 
     function ContractInfoHash(address _addr, uint32 _assetClass)
         external
-        returns (uint8, bytes32);
+        returns (uint8 contractTypeInAssetClass, bytes32 hashOfContractName);
 }
 
 interface ECR_MGR_Interface {
@@ -272,7 +263,9 @@ interface ECR_MGR_Interface {
 
     function permissiveEndEscrow(bytes32 _idxHash) external;
 
-    function retrieveEscrowOwner(bytes32 _idxHash) external returns (bytes32);
+    function retrieveEscrowOwner(bytes32 _idxHash)
+        external
+        returns (bytes32 hashOfEscrowOwnerAdress);
 
     function retrieveEscrowData(bytes32 _idxHash)
         external
