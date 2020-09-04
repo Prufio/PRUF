d158584910a1f37c2c7ffb06acf29838daba48c7
Cliff <cliff.smyth@gmail.com>
2020-09-04 10:58:58 -0400

    Added PRUF_TKN

--- PRUF_TRUFFLE/contracts/PRUF_INTERFACES.sol
+++ PRUF_TRUFFLE/contracts/PRUF_INTERFACES.sol
@@ -18,6 +18,172 @@ __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.7;
 
+interface PRUF_TKN_Interface {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    
+}
+
+interface AC_TKN_Interface {
+    function ownerOf(uint256 tokenId)
+        external
+        view
+        returns (address tokenHolderAdress);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function mintACToken(
+        address _recipientAddress,
+        uint256 _tokenId,
+        string calldata _tokenURI
+    ) external returns (uint256 tokenId);
+
+    function reMintACToken(
+        address _recipientAddress,
+        uint256 _tokenId,
+        string calldata _tokenURI
+    ) external returns (uint256 tokenId);
+
+    function name() external view returns (string memory tokenName);
+
+    function symbol() external view returns (string memory tokenSymbol);
+
+    function tokenURI(uint256 tokenId)
+        external
+        view
+        returns (string memory URI);
+}
+
+interface A_TKN_Interface {
+    function ownerOf(uint256 tokenId)
+        external
+        returns (address tokenHolderAdress);
+
+    function discard(uint256 tokenId) external;
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function mintAssetToken(
+        address _recipientAddress,
+        uint256 _tokenId,
+        string calldata _tokenURI
+    ) external returns (uint256 tokenId);
+
+    function reMintAssetToken(address _recipientAddress, uint256 _tokenId)
+        external
+        returns (uint256 tokenId);
+
+    function validateNakedToken(  //throws if authcode does not match
+        uint256 tokenId,
+        uint32 _assetClass,
+        string calldata _authCode
+    ) external;
+
+    function tokenExists(uint256 tokenId)
+        external
+        returns (uint8 uint8_Bool_type_0_170);
+
+    function name() external view returns (string memory tokenName);
+
+    function symbol() external view returns (string memory tokenSymbol);
+
+    function setURI(uint256 tokenId, string memory _tokenURI) external;
+}
+
+interface ID_TKN_Interface {
+    function ownerOf(uint256 tokenId)
+        external
+        returns (address tokenHolderAdress);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function mintPRUF_IDToken(
+        address _recipientAddress,
+        uint256 _tokenId,
+        string calldata _tokenURI
+    ) external returns (uint256 tokenId);
+
+    function reMintPRUF_IDToken(address _recipientAddress, uint256 _tokenId)
+        external
+        returns (uint256 tokenId);
+
+    function tokenExists(uint256 tokenId)
+        external
+        returns (uint8 uint8_Bool_type_0_170);
+
+    function name() external view returns (string memory tokenName);
+
+    function symbol() external view returns (string memory tokenSymbol);
+
+    function setURI(uint256 tokenId, string memory _tokenURI) external;
+
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
+
 interface AC_MGR_Interface {
     function getUserType(bytes32 _userHash, uint32 _assetClass)
         external
@@ -128,113 +294,6 @@ interface AC_MGR_Interface {
         );
 }
 
-interface AC_TKN_Interface {
-    function ownerOf(uint256 tokenId)
-        external
-        view
-        returns (address tokenHolderAdress);
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
-
-    function mintACToken(
-        address _recipientAddress,
-        uint256 _tokenId,
-        string calldata _tokenURI
-    ) external returns (uint256 tokenId);
-
-    function reMintACToken(
-        address _recipientAddress,
-        uint256 _tokenId,
-        string calldata _tokenURI
-    ) external returns (uint256 tokenId);
-
-    function name() external view returns (string memory tokenName);
-
-    function symbol() external view returns (string memory tokenSymbol);
-
-    function tokenURI(uint256 tokenId)
-        external
-        view
-        returns (string memory URI);
-}
-
-interface A_TKN_Interface {
-    function ownerOf(uint256 tokenId)
-        external
-        returns (address tokenHolderAdress);
-
-    function discard(uint256 tokenId) external;
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
-
-    function mintAssetToken(
-        address _recipientAddress,
-        uint256 _tokenId,
-        string calldata _tokenURI
-    ) external returns (uint256 tokenId);
-
-    function reMintAssetToken(address _recipientAddress, uint256 _tokenId)
-        external
-        returns (uint256 tokenId);
-
-    function validateNakedToken(  //throws if authcode does not match
-        uint256 tokenId,
-        uint32 _assetClass,
-        string calldata _authCode
-    ) external;
-
-    function tokenExists(uint256 tokenId)
-        external
-        returns (uint8 uint8_Bool_type_0_170);
-
-    function name() external view returns (string memory tokenName);
-
-    function symbol() external view returns (string memory tokenSymbol);
-
-    function setURI(uint256 tokenId, string memory _tokenURI) external;
-}
-
-interface ID_TKN_Interface {
-    function ownerOf(uint256 tokenId)
-        external
-        returns (address tokenHolderAdress);
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
-
-    function mintPRUF_IDToken(
-        address _recipientAddress,
-        uint256 _tokenId,
-        string calldata _tokenURI
-    ) external returns (uint256 tokenId);
-
-    function reMintPRUF_IDToken(address _recipientAddress, uint256 _tokenId)
-        external
-        returns (uint256 tokenId);
-
-    function tokenExists(uint256 tokenId)
-        external
-        returns (uint8 uint8_Bool_type_0_170);
-
-    function name() external view returns (string memory tokenName);
-
-    function symbol() external view returns (string memory tokenSymbol);
-
-    function setURI(uint256 tokenId, string memory _tokenURI) external;
-
-    function balanceOf(address owner) external view returns (uint256 balance);
-}
 
 interface STOR_Interface {
     function newRecord(
