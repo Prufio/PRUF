33a9f7ad049094ba03e4d9f8d59b19d6e6362e37
Cliff <cliff.smyth@gmail.com>
2020-09-16 12:09:45 -0400

    document testing plan

--- PRUF_TRUFFLE/contracts/PRUF_VERIFY.sol
+++ PRUF_TRUFFLE/contracts/PRUF_VERIFY.sol
@@ -21,11 +21,11 @@ __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
  * statuses:
  *
  * 0 = no status; clean
- * 1 = items with this SN are questionable (found an item that is apparently not real, or collisions found)
+ * 1 = items with this SN are questionable (found an item that is apparently not real)
  * 2 = items with this SN are counterfiet (original, authentic item recovered (and held/destroyed), or SN does not officially exist) (SUPER AUTH ONLY)
  * 3 = this item SN was stolen
  * 4 = this item SN was lost
- * 5 =
+ * 5 = this item SN is in process
  * 6 =
  *
  * usertypes are indicated in idxAuthInVerify[_idxHash] 
@@ -71,7 +71,7 @@ contract VERIFY is CORE {
         );
         require(
             (idxAuthInVerify[_idxHash] > 0),
-            "VFY:MOD-IA: Token IDX not listed in VERIFY approved tokens"  //must be auth or "1"
+            "VFY:MOD-IA: Token IDX not listed in VERIFY approved tokens" //must be auth or "1"
         );
         _;
     }
@@ -84,7 +84,7 @@ contract VERIFY is CORE {
      */
     function authorizeTokenForVerify(
         bytes32 _idxHash,
-        uint8 _verified,   //0 for not verify authorized, 1 for admin level auth, 2 for priveledged level auth and 3 = basic verify authorization
+        uint8 _verified, //0 for not verify authorized, 1 for admin level auth, 2 for priveledged level auth and 3 = basic verify authorization
         uint32 _assetClass
     ) external {
         AC memory ACdata = getACinfo(_assetClass);
@@ -157,6 +157,11 @@ contract VERIFY is CORE {
             items[_itemHash] != _idxHash,
             "VFY:PI:item already held by caller"
         );
+        require(
+            itemData[_itemHash].status != 2,
+            "VFY:PI:item marked counterfiet"
+        ); //check to see if item status is clean
+        require(itemData[_itemHash].status != 3, "VFY:PI:item marked stolen"); //check to see if item status is clean
         //^^^^^^^checks^^^^^^^^^
         if (items[_itemHash] != 0) {
             if (itemData[_itemHash].collisions < 4294967295)
@@ -173,8 +178,7 @@ contract VERIFY is CORE {
      * @dev:Take an item out of the (_idxHash) verify wallet
      *      the caller must posess Asset token, must pass isAuth
      *      item must be registered as "in" the callers wallet
-     *      If item is not marked as held, it will be listed as held "in" the callers wallet (return 170)
-     *      If item is marked as held in another wallet, collisions++ (return 0)
+     *      must not be lost/stolen
      */
     function takeOut(bytes32 _idxHash, bytes32 _itemHash)
         external
@@ -182,17 +186,28 @@ contract VERIFY is CORE {
         returns (uint256)
     {
         require(items[_itemHash] == _idxHash, "VFY:TO:item not held by caller"); //check to see if held by _idxHash
+         require( 
+            (itemData[_itemHash].status != 3) &&
+            (itemData[_itemHash].status != 4), "VFY:T:Item SN is marked as lost or stolen"
+        );
         //^^^^^^^checks^^^^^^^^^
 
-        items[_itemHash] = 0; // release item _asset
+        delete items[_itemHash]; // release item _asset
         //^^^^^^^effects^^^^^^^^^
 
         return 170;
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    function transfer(
-        //IS THIS A TERRIBLE IDEA? EXAMINE
+
+    /*
+     * @dev:Transfer an item out of one (_idxHash) verify wallet into another
+     *      the caller must posess Asset token, must pass isAuth
+     *      item must be registered as "in" the callers wallet
+     *      item must not be lost/stolen
+     *      destination wallet must be in same asset class as sending wallet
+     */
+    function transfer( //IS THIS A TERRIBLE IDEA? EXAMINE
         bytes32 _idxHash,
         bytes32 _newIdxHash,
         bytes32 _itemHash
@@ -204,6 +219,14 @@ contract VERIFY is CORE {
         require( //must move to same asset class
             AC_MGR.isSameRootAC(rec.assetClass, newRec.assetClass) == 170,
             "VFY:TO:Wallet is not in the same asset class"
+        );
+        require(
+            itemData[_itemHash].status != 2,
+            "VFY:PI:item marked counterfiet"
+        ); //check to see if item status is clean
+        require( 
+            (itemData[_itemHash].status != 3) &&
+            (itemData[_itemHash].status != 4), "VFY:T:Item SN is marked as lost or stolen"
         );
         //^^^^^^^checks^^^^^^^^^
 
@@ -219,7 +242,7 @@ contract VERIFY is CORE {
      *      the caller must posess Asset token, must pass isAuth and user must be auth as a "1" in that AC
      *      item must be listed as "in" the callers wallet
      */
-    function markItem(
+    function AdminMarkItem(
         bytes32 _idxHash,
         bytes32 _itemHash,
         uint8 _status,
@@ -229,7 +252,7 @@ contract VERIFY is CORE {
 
         require(
             (A_TKN.ownerOf(tokenId) == msg.sender) && //msg.sender is token holder
-                (idxAuthInVerify[_idxHash] == 3), //token is auth amdmin 
+                (idxAuthInVerify[_idxHash] == 3), //token is auth amdmin
             "VFY:MI: Caller does not hold token or is not authorized as a admin user (type1) in the asset class"
         );
         require(items[_itemHash] == _idxHash, "VFY:MI:item not held by caller"); //check to see if held by _idxHash
@@ -239,12 +262,12 @@ contract VERIFY is CORE {
         return 170;
     }
 
-        /*
+    /*
      * @dev:Mark an item with lost or stolen status (see docs at top of contract)
      *      the caller must posess Asset token, must pass isAuth and user must be auth as a "1" in that AC
      *      item must be listed as "in" the callers wallet
      */
-    function markLS(
+    function markItem(
         bytes32 _idxHash,
         bytes32 _itemHash,
         uint8 _status,
@@ -252,13 +275,24 @@ contract VERIFY is CORE {
     ) external isAuthorized(_idxHash) returns (uint256) {
         uint256 tokenId = uint256(_idxHash);
 
-        require((_status == 3) || (_status == 4) || (_status == 0), "VFY:MILS:must set to L/S 3 || 4 || 0 only"); //verify _status is l/s
+        require(
+            (_status == 3) || (_status == 4) || (_status == 5) || (_status == 1) || (_status == 0),
+            "VFY:MI:must set to 0 || 1 || 3 || 4 || 5 only"
+        ); //verify _status is l/s
         require(
             (A_TKN.ownerOf(tokenId) == msg.sender) && //msg.sender is token holder
                 (idxAuthInVerify[_idxHash] > 1), //token is auth privelidged+
             "VFY:MILS: Caller does not hold token or is not authorized as a verified user (>= 2) in the asset class"
         );
-        require(items[_itemHash] == _idxHash, "VFY:MILS:item not held by caller"); //check to see if held by _idxHash
+        require(
+            (itemData[_itemHash].status != 2) &&
+            (itemData[_itemHash].status != 5), "VFY:MLS:Item SN is marked as countefiet or in process"
+        );
+
+        require(
+            items[_itemHash] == _idxHash,
+            "VFY:MILS:item not held by caller"
+        ); //check to see if held by _idxHash
 
         itemData[_itemHash].status = _status;
         itemData[_itemHash].value = _value;
@@ -293,25 +327,60 @@ contract VERIFY is CORE {
 /*-----------------------------------------------------------------
  Make a 2 Verify AC's (custody type 4)
  make 2 assets in each new AC (1,2) (3,4)
- verify all 4 Assets as pruf verify wallets
- make wallet 
+ verify all 4 Assets as pruf verify wallets | 1 = 1 | 2 = 1 | 3 = 2 | 4 = 3 |
 
- put an item (A) in wallet 1 (should succeed) 
- try to put the same item is wallet 2 (should fail)
+ put an item A in wallet 1 (should succeed)
+ put an item B in wallet 1 (should succeed)
+ try to put item A in wallet 2 (should fail) -- item already held, collision ++
  take item A out of wallet 1 (should succeed)
  put item A in wallet 2 (should succeed) 
- transfer item A to wallet 3 (should fail)
+ transfer item A to wallet 3 (should fail)  -- non matching asset class
  transfer item A to wallet 1 (should succeed)
  take item A out of wallet 1 (should succeed)
  safePutIn item A to wallet 2 with collision threshold at 1 (should succeed)
- put item A to wallet 3 (should fail)
- take item A out of wallet 1 (should fail)
+ put item A to wallet 1 (should fail) -- item already held, collision ++
+ take item A out of wallet 1 (should fail) -- not held in wallet
  take item A out of wallet 2 (should succeed)
- safePutIn item A to wallet 2 with collision threshold at 1 (should Fail)
+ safePutIn item A to wallet 2 with collision threshold at 1 (should Fail) -- excess collisions
  safePutIn item A to wallet 3 with collision threshold at 2 (should succeed)
- transfer item A to wallet 1 (should fail)
+ transfer item A to wallet 1 (should fail) -- non matching asset class
  transfer item A to wallet 4 (should succeed)
 
- 
 
+ AdminMark item A as counterfiet (2) (should succeed)
+ transfer item A to wallet 3 (should fail) -- counterfiet
+
+ mark item A as in process (5)  (should succeed)
+ transfer item A to wallet 3 (should succeed)
+
+ mark item A as counterfiet (2) (should fail) -- not auth
+ mark item A as lost (4) (should fail) -- in process
+ take item A out of wallet 3 (should succeed)
+
+ safePutIn item A to wallet 4 with collision threshold at 2 (should Fail) -- not clean
+ PutIn item A to wallet 4 (should succeed)
+ mark item A as clean (0) (should succeed)
+ safePutIn item A to wallet 1 with collision threshold at 1 (should Fail) -- excess collisions
+ safePutIn item A to wallet 1 with collision threshold at 2 (should succeed)
+ PutIn item B to wallet 4 (should fail) -- item already held, collision ++
+ take item B out of wallet 1 (should succeed)
+
+ retrieve both items. 
+ item A should be status 0, collisions 2
+ item B should be status 0, collisions 1
+
+
+ * 0 = no status; clean
+ * 1 = items with this SN are questionable (found an item that is apparently not real)
+ * 2 = items with this SN are counterfiet (original, authentic item recovered (and held/destroyed), or SN does not officially exist) (SUPER AUTH ONLY)
+ * 3 = this item SN was stolen
+ * 4 = this item SN was lost
+ * 5 = this item SN is in process
+ * 6 =
+ *
+ * usertypes are indicated in idxAuthInVerify[_idxHash] 
+ * 0 not authorized
+ * 1 basic auth
+ * 2 priveledged level auth
+ * 3 admin level auth
  *---------------------------------------------------------------*/
