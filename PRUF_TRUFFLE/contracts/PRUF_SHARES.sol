81cdcbfb9ae5a2435cc325e76c44a38e8ec8e2d1
Cliff <cliff.smyth@gmail.com>
2020-09-21 12:56:28 -0400

    flattened  pullPayment into shares

--- PRUF_TRUFFLE/contracts/PRUF_SHARES.sol
+++ PRUF_TRUFFLE/contracts/PRUF_SHARES.sol
@@ -23,10 +23,15 @@ import "./Imports/access/Ownable.sol";
 import "./Imports/utils/Pausable.sol";
 import "./Imports/utils/ReentrancyGuard.sol";
 import "./Imports/utils/Address.sol";
-//import "./Imports/math/Safemath.sol";
-import "./Imports/payment/PullPayment.sol";
+import "./Imports/payment/escrow/Escrow.sol";
 
-contract SHARES is ReentrancyGuard, Ownable, Pausable, PullPayment {
+contract SHARES is ReentrancyGuard, Ownable, Pausable {
+
+    Escrow private _escrow;
+
+    constructor () internal {
+        _escrow = new Escrow();
+    }
 
     using Address for address payable;
     using SafeMath for uint256;
@@ -37,28 +42,18 @@ contract SHARES is ReentrancyGuard, Ownable, Pausable, PullPayment {
     address internal STOR_Address;
     STOR_Interface internal STOR;
 
-//    takes eth. at end of period (>= 1 month?) checks balance. 
-
 uint256 private maxSupply =  100;  //set max supply (100000?)
 uint256 private payPeriod =  5 minutes;  //set to 30 days
 
 uint256 private nextPayDay = block.timestamp.add(payPeriod);
-uint256 private lastPayDay;
-uint256 private oldBalance;
+uint256 private lastPayDay = block.timestamp;
+
 uint256 private newBalance;
 
 uint256 private newDividend;
-uint256 private oldDividend;
 
 uint256 internal held;
 
-
-
- struct ShareData{
-        // uint256 balance;
-        uint256 date;
-        //uint256 dividend;
-    }
 mapping(uint256 => uint256) private tokenPaymentDate; // Main Data Storage
 
 
@@ -107,7 +102,7 @@ mapping(uint256 => uint256) private tokenPaymentDate; // Main Data Storage
 
             newBalance = address(this).balance.sub(held);  //the new balance is the total balance less the amount held back for payment reservations
 
-            oldDividend = newDividend; //update old diviend to the last new dividend
+            //oldDividend = newDividend; //update old diviend to the last new dividend
 
             newDividend = newBalance.div(maxSupply);  //calculate the dividend per share of the last interval's reciepts
 
@@ -147,30 +142,48 @@ mapping(uint256 => uint256) private tokenPaymentDate; // Main Data Storage
     }
 
     function withdrawFunds(address payable payee) public {
+
         held = held.sub(payments(payee));
         withdrawPayments(payee);
     }
 
 
-    // function Balance (uint256 tokenId) external view returns (uint256) {
-    //     return tokenPaymentDate[tokenId].balance;
-    // }
+    /**
+     * @dev Withdraw accumulated payments, forwarding all gas to the recipient.
+     *
+     * Note that _any_ account can call this function, not just the `payee`.
+     * This means that contracts unaware of the `PullPayment` protocol can still
+     * receive funds this way, by having a separate account call
+     * {withdrawPayments}.
+     *
+     * WARNING: Forwarding all gas opens the door to reentrancy vulnerabilities.
+     * Make sure you trust the recipient, or are either following the
+     * checks-effects-interactions pattern or using {ReentrancyGuard}.
+     *
+     * @param payee Whose payments will be withdrawn.
+     */
+    function withdrawPayments(address payable payee) internal {
+        _escrow.withdraw(payee);
+    }
 
-    // function Withdraw (uint256 tokenId, address payable tokenHolder) external returns (uint256) {
-    //     require(
-    //         SHAR_TKN.ownerOf(tokenId) == tokenHolder, "PS:PM:caller does not hold token"
-    //     );
-        
-    //     uint256 toSend = tokenPaymentDate[tokenId].balance;
-    //     assert (toSend <= held); // owed to token cannot be > what is held
+    /**
+     * @dev Returns the payments owed to an address.
+     * @param dest The creditor's address.
+     */
+    function payments(address dest) public view returns (uint256) {
+        return _escrow.depositsOf(dest);
+    }
 
-    //     tokenPaymentDate[tokenId].balance = 0; //reset token balance to zero
-
-    //     held = held.sub(toSend); // deduct payment from "held"
-
-    //     tokenHolder.sendValue(toSend); //send payment
-
-    //     assert (tokenPaymentDate[tokenId].balance == 0); //token balance must now be zero
-    // }
+    /**
+     * @dev Called by the payer to store the sent amount as credit to be pulled.
+     * Funds sent in this way are stored in an intermediate {Escrow} contract, so
+     * there is no danger of them being spent before withdrawal.
+     *
+     * @param dest The destination address of the funds.
+     * @param amount The amount to transfer.
+     */
+    function _asyncTransfer(address dest, uint256 amount) internal {
+        _escrow.deposit{ value: amount }(dest);
+    }
 
 }
