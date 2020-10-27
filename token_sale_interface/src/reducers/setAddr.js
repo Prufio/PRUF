2e0d02415b0a918e62667a1cd989c84f51564bc7
Cliff <cliff.smyth@gmail.com>
2020-10-26 20:54:51 -0400

    Remove web stuff

--- token_sale_interface/src/reducers/setAddr.js
+++
@@ -1,8 +0,0 @@
-const globalAddrReducer = (state = '', action) => {
-    switch(action.type){
-        case 'SET_ADDR': return action.payload.addr;
-        default : return state;
-    }
-}
-
-export default globalAddrReducer
--- token_sale_interface/src/reducers/web3.js
+++
@@ -1,8 +0,0 @@
-const globalWeb3Reducer = (state = '', action) => {
-    switch(action.type){
-        case 'SET_WEB3': return action.payload.web3;
-        default : return state;
-    }
-}
-
-export default globalWeb3Reducer
