66886a8535ca024a4b9598bd65d3e3b1989f449d
Cliff <cliff.smyth@gmail.com>
2020-12-01 11:57:28 -0400

    Gas optimizations, code clarity

--- PRUF_TRUFFLE/contracts/PRUF_UTIL_TKN.sol
+++ PRUF_TRUFFLE/contracts/PRUF_UTIL_TKN.sol
@@ -20,9 +20,7 @@ pragma solidity ^0.6.7;
 
 import "./PRUF_INTERFACES.sol";
 import "./Imports/access/AccessControl.sol";
-//import "./Imports/token/ERC20/ERC20.sol";
 import "./Imports/token/ERC20/ERC20Burnable.sol";
-//import "./Imports/token/ERC20/ERC20Pausable.sol";
 import "./Imports/utils/Pausable.sol";
 import "./Imports/token/ERC20/ERC20Snapshot.sol";
 
@@ -51,7 +49,6 @@ contract UTIL_TKN is
     Context,
     AccessControl,
     ERC20Burnable,
-    //   ERC20Pausable,
     Pausable,
     ERC20Snapshot
 {
@@ -76,8 +73,10 @@ contract UTIL_TKN is
     }
 
     uint256 trustedAgentEnabled = 1;
-    
-    mapping (address => uint256) private coldWallet;
+
+    uint256 airDropAmount;
+
+    mapping(address => uint256) private coldWallet;
 
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
@@ -87,60 +86,131 @@ contract UTIL_TKN is
      */
     constructor() public ERC20("PRuF_TKN", "PRuF") {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(SNAPSHOT_ROLE, _msgSender());
         _setupRole(MINTER_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
-        _setupRole(PAYABLE_ROLE, _msgSender());
+    }
+
+    //------------------------------------------------------------------------MODIFIERS
+
+    /*
+     * @dev Verify user credentials
+     * Originating Address:
+     *      is Admin
+     */
+    modifier isAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "PRUF:MOD: must have DEFAULT_ADMIN_ROLE"
+        );
+        _;
     }
 
     /*
-     * @dev PERMENANTLY !!!  Kill trusted agent and payable functions
+     * @dev Verify user credentials
+     * Originating Address:
+     *      is Pauser
      */
-    function killTrustedAgent(uint256 _key) external {  //---------------------------------------------------DPS:TEST : NEW
+    modifier isPauser() {
         require(
-            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-            "PRuF:SSA: must have DEFAULT_ADMIN_ROLE"
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "PRUF:MOD: must have PAUSER_ROLE"
         );
+        _;
+    }
 
-        if (_key == 170){ 
-            trustedAgentEnabled = 0;  //--------------------------------------------------THIS IS A PERMANENT ACTION AND CANNOT BE UNDONE
+    /*
+     * @dev Verify user credentials
+     * Originating Address:
+     *      is Minter
+     */
+    modifier isMinter() {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "PRUF:MOD: must have MINTER_ROLE"
+        );
+        _;
+    }
+
+    /*
+     * @dev Verify user credentials
+     * Originating Address:
+     *      is Payable in pruf
+     */
+    modifier isPayable() {
+        require(
+            hasRole(PAYABLE_ROLE, _msgSender()),
+            "PRUF:MOD: must have PAYABLE_ROLE"
+        );
+        require( //---------------------------------------------------DPS:TEST : NEW
+            trustedAgentEnabled == 1,
+            "PRUF:MOD: Trusted Payable Function permanently disabled - use allowance / transferFrom pattern"
+        );
+        _;
+    }
+
+    /*
+     * @dev Verify user credentials
+     * Originating Address:
+     *      is Trusted Agent
+     */
+    modifier isTrustedAgent() {
+        require(
+            hasRole(TRUSTED_AGENT_ROLE, _msgSender()),
+            "PRUF:MOD: must have TRUSTED_AGENT_ROLE"
+        );
+        require( //---------------------------------------------------DPS:TEST : NEW
+            trustedAgentEnabled == 1,
+            "PRUF:MOD: Trusted Agent function permanently disabled - use allowance / transferFrom pattern"
+        );
+        _;
+    }
+
+    /*
+     * @dev PERMANANTLY !!!  Kill trusted agent and payable functions
+     * this will break functionality of current payment mechanisms.
+     */
+    function adminKillTrustedAgent(uint256 _key) external isAdmin {
+        //---------------------------------------------------DPS:TEST : NEW
+        if (_key == 170) {
+            trustedAgentEnabled = 0; //-------------------THIS IS A PERMANENT ACTION AND CANNOT BE UNDONE
         }
     }
 
     /*
      * @dev Set calling wallet to a "cold Wallet" that cannot be manipulated by TRUSTED_AGENT or PAYABLE permissioned functions
+     * WALLET ADRESSES SET TO "Cold" DO NOT WORK WITH TRUSTED_AGENT FUNCTIONS
      */
-    function setColdWallet() external {  //---------------------------------------------------DPS:TEST : NEW
+    function setColdWallet() external {
+        //---------------------------------------------------DPS:TEST : NEW
         coldWallet[msg.sender] = 170;
     }
 
     /*
      * @dev un-set calling wallet to a "cold Wallet", enabling manipulation by TRUSTED_AGENT and PAYABLE permissioned functions
+     * WALLET ADRESSES SET TO "Cold" DO NOT WORK WITH TRUSTED_AGENT FUNCTIONS
      */
-    function unSetColdWallet() external {  //---------------------------------------------------DPS:TEST : NEW
+    function unSetColdWallet() external {
+        //---------------------------------------------------DPS:TEST : NEW
         coldWallet[msg.sender] = 0;
     }
 
     /*
      * @dev return an adresses "cold wallet" status
+     * WALLET ADRESSES SET TO "Cold" DO NOT WORK WITH TRUSTED_AGENT FUNCTIONS
      */
-    function isColdWallet (address _addr) external view returns (uint256){
+    function isColdWallet(address _addr) external view returns (uint256) {
         return coldWallet[_addr];
     }
 
     /*
      * @dev Set adress of SHARES payment contract. by default contract will use root adress instead if set to zero.
      */
-    function AdminSetSharesAddress(address _paymentAddress) external {
+    function AdminSetSharesAddress(address _paymentAddress) external isAdmin {
         require(
             _paymentAddress != address(0),
             "PRuF:SSA: payment address cannot be zero"
         );
 
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-            "PRuF:SSA: must have DEFAULT_ADMIN_ROLE"
-        );
         //^^^^^^^checks^^^^^^^^^
 
         sharesAddress = _paymentAddress;
@@ -156,60 +226,44 @@ contract UTIL_TKN is
         uint256 _rootPrice,
         address _ACTHaddress,
         uint256 _ACTHprice
-    ) external {
-        require(
-            hasRole(PAYABLE_ROLE, _msgSender()),
-            "PRuF:PPS: must have PAYABLE_ROLE"
-        );
-        require( //---------------------------------------------------DPS:TEST : NEW
-            trustedAgentEnabled == 0,
-            "PRuF:PPS: Function permanently disabled - use allowance / transferFrom pattern"
-        );
+    ) external isPayable {
         require( //---------------------------------------------------DPS:TEST : NEW
             coldWallet[_senderAddress] == 0,
-            "PRuF:PPS: Cold Wallet - Trusted functions prohibited"
+            "PRuF:PPS: Cold Wallet - Trusted payable functions prohibited"
         );
         require( //redundant? throws on transfer?
             balanceOf(_senderAddress) >= _rootPrice.add(_ACTHprice),
             "PRuF:PPS: insufficient balance"
         );
         //^^^^^^^checks^^^^^^^^^
-        //-------------------------------------------------------------SHARES specific code
-        address _sharesAddress = sharesAddress;
 
-        if (sharesAddress == address(0)) {
-            _sharesAddress = _rootAddress;
+        if (sharesAddress == address(0)) { //IF SHARES ADDRESS IS NOT SET
+            _transfer(_senderAddress, _rootAddress, _rootPrice);
+            _transfer(_senderAddress, _ACTHaddress, _ACTHprice);
+        } else { //IF SHARES ADDRESS IS SET
+            uint256 sharesShare = _rootPrice.div(uint256(4)); // sharesShare is 0.25 share of root costs
+            uint256 rootShare = _rootPrice.sub(sharesShare); // adjust root price to be root price - 0.25 share
+
+            _transfer(_senderAddress, _rootAddress, rootShare);
+            _transfer(_senderAddress, sharesAddress, sharesShare);
+            _transfer(_senderAddress, _ACTHaddress, _ACTHprice);
         }
-
-        uint256 sharesShare = _rootPrice.div(uint256(4)); //SHARES
-        uint256 rootShare = _rootPrice.sub(sharesShare); //SHARES
-
-        _transfer(_senderAddress, _rootAddress, rootShare);
-        _transfer(_senderAddress, _sharesAddress, sharesShare);
-        _transfer(_senderAddress, _ACTHaddress, _ACTHprice);
-
-        //-------------------------------------------------------------SHARES specific code
         //^^^^^^^effects / interactions^^^^^^^^^
     }
 
     /*
      * @dev arbitrary burn (requires TRUSTED_AGENT_ROLE)   ****USE WITH CAUTION
      */
-    function trustedAgentBurn(address _addr, uint256 _amount) public {
-        require(
-            hasRole(TRUSTED_AGENT_ROLE, _msgSender()),
-            "PRuF:BRN: must have TRUSTED_AGENT_ROLE"
-        );
+    function trustedAgentBurn(address _addr, uint256 _airDropAmount)
+        public
+        isTrustedAgent
+    {
         require( //---------------------------------------------------DPS:TEST : NEW
-            trustedAgentEnabled == 0,
-            "PRuF:BRN: Function permanently disabled - use allowance / transferFrom pattern"
-        );
-        require(//---------------------------------------------------DPS:TEST : NEW
             coldWallet[_addr] == 0,
             "PRuF:BRN: Cold Wallet - Trusted functions prohibited"
         );
         //^^^^^^^checks^^^^^^^^^
-        _burn(_addr, _amount);
+        _burn(_addr, _airDropAmount);
         //^^^^^^^effects^^^^^^^^^
     }
 
@@ -219,22 +273,14 @@ contract UTIL_TKN is
     function trustedAgentTransfer(
         address _from,
         address _to,
-        uint256 _amount
-    ) public {
-        require(
-            hasRole(TRUSTED_AGENT_ROLE, _msgSender()),
-            "PRuF:TAT: must have TRUSTED_AGENT_ROLE"
-        );
+        uint256 _airDropAmount
+    ) public isTrustedAgent {
         require( //---------------------------------------------------DPS:TEST : NEW
-            trustedAgentEnabled == 0,
-            "PRuF:TAT: Function permanently disabled - use allowance / transferFrom pattern"
-        );
-        require(//---------------------------------------------------DPS:TEST : NEW
             coldWallet[_from] == 0,
             "PRuF:TAT: Cold Wallet - Trusted functions prohibited"
         );
         //^^^^^^^checks^^^^^^^^^
-        _transfer(_from, _to, _amount);
+        _transfer(_from, _to, _airDropAmount);
         //^^^^^^^effects^^^^^^^^^
     }
 
@@ -250,7 +296,7 @@ contract UTIL_TKN is
     }
 
     /**
-     * @dev Creates `amount` new tokens for `to`.
+     * @dev Creates `airDropAmount` new tokens for `to`.
      *
      * See {ERC20-_mint}.
      *
@@ -258,13 +304,9 @@ contract UTIL_TKN is
      *
      * - the caller must have the `MINTER_ROLE`.
      */
-    function mint(address to, uint256 amount) public virtual {
+    function mint(address to, uint256 amount) public virtual isMinter {
         require(
-            hasRole(MINTER_ROLE, _msgSender()),
-            "PRuF:M: must have minter role to mint"
-        );
-        require(
-            amount.add(totalSupply()) <= maxSupply,
+            airDropAmount.add(totalSupply()) <= maxSupply,
             "PRuF:M: mint request exceeds max supply"
         );
         //^^^^^^^checks^^^^^^^^^
@@ -282,11 +324,7 @@ contract UTIL_TKN is
      *
      * - the caller must have the `PAUSER_ROLE`.
      */
-    function pause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "PRuF:P: must have pauser role to pause"
-        );
+    function pause() public virtual isPauser {
         //^^^^^^^checks^^^^^^^^^
         _pause();
         //^^^^^^^effects^^^^^^^^
@@ -301,17 +339,13 @@ contract UTIL_TKN is
      *
      * - the caller must have the `PAUSER_ROLE`.
      */
-    function unpause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "PRuF:U: must have pauser role to unpause"
-        );
+    function unpause() public virtual isPauser {
         //^^^^^^^checks^^^^^^^^^
         _unpause();
         //^^^^^^^effects^^^^^^^^
     }
 
-    function _beforeTokenTransfer(
+    function _beforeTokenTransfer( //all paused functions are blocked here, unless caller has "pauser" role
         address from,
         address to,
         uint256 amount
