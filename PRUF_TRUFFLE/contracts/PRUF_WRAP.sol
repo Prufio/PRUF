a292b3e5f25600b0a250576fff63972c912f6024
Cliff <cliff.smyth@gmail.com>
2021-02-09 20:59:06 -0400

    WRAP ready for testing

--- PRUF_TRUFFLE/contracts/PRUF_WRAP.sol
+++ PRUF_TRUFFLE/contracts/PRUF_WRAP.sol
@@ -12,11 +12,9 @@ __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
 
 /*-----------------------------------------------------------------
  *  TO DO --- complete test! DPS TEST NEW CONTRACT
- *  UNFINISHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *  MUST BE TRUSTED AGENT IN A_TKN
  *
  *----------------------------------------------------------------*/
-
-
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.7;
@@ -25,21 +23,18 @@ import "./PRUF_CORE.sol";
 import "./Imports/token/ERC721/IERC721.sol";
 
 contract WRAP is CORE {
-    // modifier isAuthorized(bytes32 _idxHash) override {
-    //     //require that user is authorized and token is held by contract
-    //     uint256 tokenId = uint256(_idxHash);
-    //     require(
-    //         (A_TKN.ownerOf(tokenId) == address(this)),
-    //         "A:MOD-IA: EXTEND contract does not hold token"
-    //     );
-    //     _;
-    // }
+    struct WrappedToken {
+        uint256 tokenID;
+        address tokenContract;
+    }
+
+    mapping(uint256 => WrappedToken) private wrapped; // pruf tokenID -> original TokenID, ContractAddress
 
     modifier isTokenHolder(uint256 _tokenID, address _tokenContract) {
         //require that user holds token @ ID-Contract
         require(
             (IERC721(_tokenContract).ownerOf(_tokenID) == _msgSender()),
-            "A:MOD-IA: caller does not hold specified token"
+            "WRAP:MOD-IA: caller does not hold specified token"
         );
         _;
     }
@@ -53,11 +48,11 @@ contract WRAP is CORE {
      * Makes a pruf record (exists?) if so does not change
      * Mints a pruf token to caller (exists?) if so ???????
      * Asset Class? must be type 5  / enabled for contract address
-     * 
+     *
      */
     function wrap721(
-        uint256 _tokenID,
-        address _tokenContract,
+        uint256 _foreignTokenID,
+        address _foreignTokenContract,
         bytes32 _rgtHash,
         uint32 _assetClass,
         uint32 _countDownStart
@@ -65,93 +60,104 @@ contract WRAP is CORE {
         external
         nonReentrant
         whenNotPaused
-        isTokenHolder(_tokenID, _tokenContract)
+        isTokenHolder(_foreignTokenID, _foreignTokenContract) // without this, the dark forest gets it!
     {
-        bytes32 idxHash = keccak256(abi.encodePacked(_tokenID, _tokenContract));
+        bytes32 idxHash =
+            keccak256(abi.encodePacked(_foreignTokenID, _foreignTokenContract));
+
         Record memory rec = getRecord(idxHash);
         AC memory AC_info = getACinfo(_assetClass);
-        AC memory oldAC_info = getACinfo(rec.assetClass);
+
+        uint256 newTokenId = uint256(idxHash);
+        // AC memory oldAC_info = getACinfo(rec.assetClass);
 
         require(
             AC_info.custodyType == 5,
-            "E:DEC:Asset class.custodyType must be 5 (wrapped/decorated erc721)"
+            "WRAP:WRP:Asset class.custodyType must be 5 (wrapped/decorated erc721)"
         );
         require(
-            (AC_info.extendedData == uint160(_tokenContract)) || (AC_info.extendedData == 0) ,
-            "E:DEC:Asset class extended data must be '0' or uint160(ERC721 contract address)"
+            (AC_info.extendedData == uint160(_foreignTokenContract)) ||
+                (AC_info.extendedData == 0),
+            "WRAP:WRP:Asset class extended data must be '0' or uint160(ERC721 contract address)"
         );
+        //^^^^^^^checks^^^^^^^^^
 
+        wrapped[newTokenId].tokenID = _foreignTokenID;
+        wrapped[newTokenId].tokenContract = _foreignTokenContract;
         //^^^^^^^effects^^^^^^^^^
 
-        if (AC_info.assetClassRoot == oldAC_info.assetClassRoot) { //if record has existed before
-            createRecord(
-                idxHash,
-                _rgtHash,
-                _assetClass,
-                rec.countDownStart
-            );
-        } else {
+        foreignTransfer(
+            _foreignTokenContract,
+            _msgSender(),
+            address(this),
+            _foreignTokenID
+        ); // move token to this contract
+
+        if (rec.assetClass == 0) {
+            //record does not exist
             createRecord(idxHash, _rgtHash, _assetClass, _countDownStart);
+        } else {
+            //just mint the token, record already exists
+            A_TKN.mintAssetToken(_msgSender(), newTokenId, "pruf.io");
         }
         deductServiceCosts(_assetClass, 1);
-
         //^^^^^^^interactions^^^^^^^^^
     }
 
     /*
      * @dev Unwraps a token, returns original to caller
-     * caller holds matching token?
-     * burns pruf token
+     * burns pruf token from caller wallet
      * Sends original 721 to caller
      */
-    function unWrap721(
-        uint256 _tokenID,
-        address _tokenContract,
-        bytes32 _rgtHash,
-        uint32 _assetClass,
-        uint32 _countDownStart
-    )
+    function unWrap721(uint256 _tokenID)
         external
         nonReentrant
         whenNotPaused
-        isTokenHolder(_tokenID, _tokenContract)
+        isTokenHolder(_tokenID, A_TKN_Address) //caller holds the wrapped token
     {
-        bytes32 idxHash = keccak256(abi.encodePacked(_tokenID, _tokenContract));
+        bytes32 idxHash = bytes32(_tokenID);
         Record memory rec = getRecord(idxHash);
-        AC memory AC_info = getACinfo(_assetClass);
-        AC memory oldAC_info = getACinfo(rec.assetClass);
+        AC memory AC_info = getACinfo(rec.assetClass);
+        address foreignTokenContract = wrapped[_tokenID].tokenContract;
+        uint256 foreignTokenID = wrapped[_tokenID].tokenID;
 
         require(
             AC_info.custodyType == 5,
-            "E:DEC:Asset class.custodyType must be 5 (wrapped/decorated erc721)"
+            "WRAP:UNWRP:Asset class.custodyType must be 5 (wrapped/decorated erc721)"
         );
         require(
-            (AC_info.extendedData == uint160(_tokenContract)) || (AC_info.extendedData == 0) ,
-            "E:DEC:Asset class extended data must be '0' or uint160(ERC721 contract address)"
+            (AC_info.extendedData == uint160(foreignTokenContract)) ||
+                (AC_info.extendedData == 0),
+            "WRAP:UNWRP:Asset class extended data must be '0' or uint160(ERC721 contract address)"
         );
+        require(
+            rec.assetStatus == 51,
+            "WRAP:UNWRP:Asset not in transferrable status"
+        );
+        //^^^^^^^checks^^^^^^^^^
 
         //^^^^^^^effects^^^^^^^^^
 
-        if (AC_info.assetClassRoot == oldAC_info.assetClassRoot) { //if record has existed before
-            createRecord(
-                idxHash,
-                _rgtHash,
-                _assetClass,
-                rec.countDownStart
-            );
-        } else {
-            createRecord(idxHash, _rgtHash, _assetClass, _countDownStart);
-        }
-        deductServiceCosts(_assetClass, 1);
+        A_TKN.trustedAgentBurn(_tokenID);
 
+        foreignTransfer(
+            foreignTokenContract,
+            address(this),
+            _msgSender(),
+            foreignTokenID
+        );
         //^^^^^^^interactions^^^^^^^^^
     }
 
-
-    function foreignTransfer(address _tokenContract, address _from, address _to, uint256 _tokenId) public {
-        IERC721(_tokenContract).safeTransferFrom(_from, _to, _tokenId);
+    /*
+     * @dev transfer a foreign token
+     */
+    function foreignTransfer(
+        address _tokenContract,
+        address _from,
+        address _to,
+        uint256 _tokenID
+    ) internal {
+        IERC721(_tokenContract).safeTransferFrom(_from, _to, _tokenID);
     }
-
- 
-
 }
