02cdf87eb190636d6f71916ad37cf4214de5a3ca
Cliff <cliff.smyth@gmail.com>
2021-02-18 19:23:49 -0400

    Added unchecked to uinttostring

--- PRUF_TRUFFLE/contracts/PRUF_PIP.sol
+++ PRUF_TRUFFLE/contracts/PRUF_PIP.sol
@@ -140,15 +140,19 @@ contract PIP is CORE {
         }
         uint256 temp = value;
         uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
+        unchecked {
+            while (temp != 0) {
+                digits++;
+                temp /= 10;
+            }
         }
         bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
+        unchecked {
+            while (value != 0) {
+                digits -= 1;
+                buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+                value /= 10;
+            }
         }
         return string(buffer);
         // return "pip";
