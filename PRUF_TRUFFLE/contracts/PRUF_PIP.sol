95117d8094234e259d6cf5016b513262120d49d8
Cliff <cliff.smyth@gmail.com>
2021-02-18 19:11:02 -0400

    update new uinttostring

--- PRUF_TRUFFLE/contracts/PRUF_PIP.sol
+++ PRUF_TRUFFLE/contracts/PRUF_PIP.sol
@@ -100,33 +100,67 @@ contract PIP is CORE {
         //^^^^^^^interactions^^^^^^^^
     }
 
-    function uint256toString(uint256 number)
-        public
-        pure
-        returns (string memory)
-    {
+    // function uint256toString(uint256 number)
+    //     public
+    //     pure
+    //     returns (string memory)
+    // {
+    //     // Inspired by OraclizeAPI's implementation - MIT licence
+    //     // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+    //     // shamelessly jacked straight outa OpenZepplin  openzepplin.org
+
+    //     if (number == 0) {
+    //         return "0";
+    //     }
+    //     uint256 temp = number;
+    //     uint256 digits;
+    //     while (temp != 0) {
+    //         digits++;
+    //         temp /= 10;
+    //     }
+    //     bytes memory buffer = new bytes(digits);
+    //     uint256 index = digits - 1;
+    //     temp = number;
+    //     while (temp != 0) {
+    //         buffer[index--] = bytes1(uint8(48 + (temp % 10)));
+    //         temp /= 10;
+    //     }
+    //     return string(buffer);
+    // }
+
+    function uint256toString(uint256 value) internal pure returns (string memory) {
         // Inspired by OraclizeAPI's implementation - MIT licence
         // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
         // shamelessly jacked straight outa OpenZepplin  openzepplin.org
 
-        if (number == 0) {
+        if (value == 0) {
             return "0";
         }
-        uint256 temp = number;
+        uint256 temp = value;
         uint256 digits;
         while (temp != 0) {
             digits++;
             temp /= 10;
         }
         bytes memory buffer = new bytes(digits);
-        uint256 index = digits - 1;
-        temp = number;
-        while (temp != 0) {
-            buffer[index--] = bytes1(uint8(48 + (temp % 10)));
-            temp /= 10;
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
         }
         return string(buffer);
     }
+
+
+
+
+
+
+
+
+
+
+
 
     function deductImportRecordCosts(uint32 _assetClass)
         internal
