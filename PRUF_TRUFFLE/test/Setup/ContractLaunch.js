18747fdd80936a128972e77a944f6ef3d9c4ebe9
Drake Smyth <drakesmyth@gmail.com>
2021-02-21 15:00:33 -0400

    contract launch test file

---
+++ PRUF_TRUFFLE/test/Setup/ContractLaunch.js
@@ -0,0 +1,182 @@
+/*--------------------------------------------------------PRuF0.7.1
+__/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
+ _\/\\\/////////\\\ _/\\\///////\\\ ____\//..\//____\/\\\///////////__
+  _\/\\\.......\/\\\.\/\\\.....\/\\\ ________________\/\\\ ____________
+   _\/\\\\\\\\\\\\\/__\/\\\\\\\\\\\/_____/\\\____/\\\.\/\\\\\\\\\\\ ____
+    _\/\\\/////////____\/\\\//////\\\ ___\/\\\___\/\\\.\/\\\///////______
+     _\/\\\ ____________\/\\\ ___\//\\\ __\/\\\___\/\\\.\/\\\ ____________
+      _\/\\\ ____________\/\\\ ____\//\\\ _\/\\\___\/\\\.\/\\\ ____________
+       _\/\\\ ____________\/\\\ _____\//\\\.\//\\\\\\\\\ _\/\\\ ____________
+        _\/// _____________\/// _______\/// __\///////// __\/// _____________
+         *-------------------------------------------------------------------*/
+
+        const PRUF_STOR = artifacts.require('STOR');
+        const PRUF_APP = artifacts.require('APP');
+        const PRUF_NP = artifacts.require('NP');
+        const PRUF_AC_MGR = artifacts.require('AC_MGR');
+        const PRUF_AC_TKN = artifacts.require('AC_TKN');
+        const PRUF_A_TKN = artifacts.require('A_TKN');
+        const PRUF_ID_TKN = artifacts.require('ID_TKN');
+        const PRUF_ECR_MGR = artifacts.require('ECR_MGR');
+        const PRUF_ECR = artifacts.require('ECR');
+        const PRUF_ECR2 = artifacts.require('ECR2');
+        const PRUF_APP_NC = artifacts.require('APP_NC');
+        const PRUF_NP_NC = artifacts.require('NP_NC');
+        const PRUF_ECR_NC = artifacts.require('ECR_NC');
+        const PRUF_RCLR = artifacts.require('RCLR');
+        const PRUF_PIP = artifacts.require('PIP');
+        const PRUF_HELPER = artifacts.require('Helper');
+        const PRUF_MAL_APP = artifacts.require('MAL_APP');
+        const PRUF_UTIL_TKN = artifacts.require('UTIL_TKN');
+        
+        contract('Launch', accounts => {
+        
+            console.log('//**************************BEGIN Launch**************************//')
+        
+            const account1 = accounts[0];
+        
+        
+            it('Should deploy Storage', async () => {
+                const PRUF_STOR_TEST = await PRUF_STOR.deployed({ from: account1 });
+                console.log(PRUF_STOR_TEST.address);
+                assert(PRUF_STOR_TEST.address !== '');
+                STOR = PRUF_STOR_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_APP', async () => {
+                const PRUF_APP_TEST = await PRUF_APP.deployed({ from: account1 });
+                console.log(PRUF_APP_TEST.address);
+                assert(PRUF_APP_TEST.address !== '');
+                APP = PRUF_APP_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_NP', async () => {
+                const PRUF_NP_TEST = await PRUF_NP.deployed({ from: account1 });
+                console.log(PRUF_NP_TEST.address);
+                assert(PRUF_NP_TEST.address !== '');
+                NP = PRUF_NP_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_AC_MGR', async () => {
+                const PRUF_AC_MGR_TEST = await PRUF_AC_MGR.deployed({ from: account1 });
+                console.log(PRUF_AC_MGR_TEST.address);
+                assert(PRUF_AC_MGR_TEST.address !== '');
+                AC_MGR = PRUF_AC_MGR_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_AC_TKN', async () => {
+                const PRUF_AC_TKN_TEST = await PRUF_AC_TKN.deployed({ from: account1 });
+                console.log(PRUF_AC_TKN_TEST.address);
+                assert(PRUF_AC_TKN_TEST.address !== '')
+                AC_TKN = PRUF_AC_TKN_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_A_TKN', async () => {
+                const PRUF_A_TKN_TEST = await PRUF_A_TKN.deployed({ from: account1 });
+                console.log(PRUF_A_TKN_TEST.address);
+                assert(PRUF_A_TKN_TEST.address !== '')
+                A_TKN = PRUF_A_TKN_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_ECR_MGR', async () => {
+                const PRUF_ECR_MGR_TEST = await PRUF_ECR_MGR.deployed({ from: account1 });
+                console.log(PRUF_ECR_MGR_TEST.address);
+                assert(PRUF_ECR_MGR_TEST.address !== '');
+                ECR_MGR = PRUF_ECR_MGR_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_ECR', async () => {
+                const PRUF_ECR_TEST = await PRUF_ECR.deployed({ from: account1 });
+                console.log(PRUF_ECR_TEST.address);
+                assert(PRUF_ECR_TEST.address !== '');
+                ECR = PRUF_ECR_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_APP_NC', async () => {
+                const PRUF_APP_NC_TEST = await PRUF_APP_NC.deployed({ from: account1 });
+                console.log(PRUF_APP_NC_TEST.address);
+                assert(PRUF_APP_NC_TEST.address !== '');
+                APP_NC = PRUF_APP_NC_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_NP_NC', async () => {
+                const PRUF_NP_NC_TEST = await PRUF_NP_NC.deployed({ from: account1 });
+                console.log(PRUF_NP_NC_TEST.address);
+                assert(PRUF_NP_NC_TEST.address !== '')
+                NP_NC = PRUF_NP_NC_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_ECR_NC', async () => {
+                const PRUF_ECR_NC_TEST = await PRUF_ECR_NC.deployed({ from: account1 });
+                console.log(PRUF_ECR_NC_TEST.address);
+                assert(PRUF_ECR_NC_TEST.address !== '');
+                ECR_NC = PRUF_ECR_NC_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_PIP', async () => {
+                const PRUF_PIP_TEST = await PRUF_PIP.deployed({ from: account1 });
+                console.log(PRUF_PIP_TEST.address);
+                assert(PRUF_PIP_TEST.address !== '')
+                PIP = PRUF_PIP_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_RCLR', async () => {
+                const PRUF_RCLR_TEST = await PRUF_RCLR.deployed({ from: account1 });
+                console.log(PRUF_RCLR_TEST.address);
+                assert(PRUF_RCLR_TEST.address !== '')
+                RCLR = PRUF_RCLR_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_HELPER', async () => {
+                const PRUF_HELPER_TEST = await PRUF_HELPER.deployed({ from: account1 });
+                console.log(PRUF_HELPER_TEST.address);
+                assert(PRUF_HELPER_TEST.address !== '')
+                Helper = PRUF_HELPER_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_ID_TKN', async () => {
+                const PRUF_ID_TKN_TEST = await PRUF_ID_TKN.deployed({ from: account1 });
+                console.log(PRUF_ID_TKN_TEST.address);
+                assert(PRUF_ID_TKN_TEST.address !== '')
+                ID_TKN = PRUF_ID_TKN_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_ECR2', async () => {
+                const PRUF_ECR2_TEST = await PRUF_ECR2.deployed({ from: account1 });
+                console.log(PRUF_ECR2_TEST.address);
+                assert(PRUF_ECR2_TEST.address !== '');
+                ECR2 = PRUF_ECR2_TEST;
+            })
+        
+        
+            it('Should deploy PRUF_MAL_APP', async () => {
+                const PRUF_MAL_APP_TEST = await PRUF_MAL_APP.deployed({ from: account1 });
+                console.log(PRUF_MAL_APP_TEST.address);
+                assert(PRUF_MAL_APP_TEST.address !== '')
+                MAL_APP = PRUF_MAL_APP_TEST;
+            })
+        
+        
+            it('Should deploy UTIL_TKN', async () => {
+                console.log("//**************************END Launch**************************//")
+                const PRUF_UTIL_TKN_TEST = await PRUF_UTIL_TKN.deployed({ from: account1 });
+                console.log(PRUF_UTIL_TKN_TEST.address);
+                assert(PRUF_UTIL_TKN_TEST.address !== '')
+                UTIL_TKN = PRUF_UTIL_TKN_TEST;
+            })
+})
