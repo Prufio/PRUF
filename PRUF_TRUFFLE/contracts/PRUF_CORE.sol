774e26f0dbdf71660e38a63125773f756d8a563c
Cliff <cliff.smyth@gmail.com>
2021-02-27 14:34:45 -0400

    Data Sculptor

--- PRUF_TRUFFLE/contracts/PRUF_CORE.sol
+++ PRUF_TRUFFLE/contracts/PRUF_CORE.sol
@@ -22,7 +22,7 @@ __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./PRUF_INTERFACES.sol";
+//import "./PRUF_INTERFACES.sol";
 import "./Imports/utils/ReentrancyGuard.sol";
 import "./PRUF_BASIC.sol";
 
