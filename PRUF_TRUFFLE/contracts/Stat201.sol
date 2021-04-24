de94df7f87c533d061182fa13011ffef4b39265f
Cliff <cliff.smyth@gmail.com>
2021-04-24 16:09:58 -0400

    status 201 contract

--- PRUF_TRUFFLE/contracts/Stat201.sol
+++ PRUF_TRUFFLE/contracts/Stat201.sol
@@ -11,7 +11,7 @@ __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\../\\ ___/\\\\\\\\\\\\\\\
          *-------------------------------------------------------------------*/
 
 /*-----------------------------------------------------------------
- *  TO DO
+ *  TO DO DPS:CHECK this requires this contract to be an authorized 1 in storage.
  *
  *---------------------------------------------------------------*/
 
@@ -20,12 +20,11 @@ pragma solidity ^0.8.0;
 
 import "./PRUF_CORE.sol";
 
-contract RCLR is CORE {
+contract STAT201 is CORE {
 
-    //--------------------------------------------External Functions--------------------------
 
     /*
-     * @dev //gets item out of recycled status -- caller is assetToken contract
+     * @dev //Sets an item to reserved status 201 when called, if record links to an invalid storage type. Stat201 allows a rewrite of IPFS2
      */
     function set201(bytes32 _idxHash)
         external
@@ -35,20 +34,20 @@ contract RCLR is CORE {
     {
         Record memory rec = getRecord(_idxHash);
         AC memory AC_info = getACinfo(rec.assetClass);
-        uint256 stat =AC_MGR.getStorageProviderStatus(AC_info.storageProvider);
+        uint256 storageProviderStatus =AC_MGR.getStorageProviderStatus(AC_info.storageProvider);
 
         require
-            (stat == 0,
+            (storageProviderStatus == 0,
             "S201:201: Record must have an invalid storage type to set stat 201"
         );
         //^^^^^^^checks^^^^^^^^^
-
         rec.assetStatus = 201;
         //^^^^^^^effects^^^^^^^^^
+
         writeRecord(_idxHash, rec);
         //^^^^^^^interactions^^^^^^^^^
-    
-
         
     }
+
+
 }
