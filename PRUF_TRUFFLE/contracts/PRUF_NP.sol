a9ad17b087cd102239f23f374d97743f03ad8a88
Cliff <cliff.smyth@gmail.com>
2021-07-19 15:42:49 -0400

    Remove unneeded returns, docs

--- PRUF_TRUFFLE/contracts/PRUF_NP.sol
+++ PRUF_TRUFFLE/contracts/PRUF_NP.sol
@@ -1,4 +1,4 @@
-/*--------------------------------------------------------PRüF0.8.0
+/**--------------------------------------------------------PRüF0.8.0
 __/\\\\\\\\\\\\\ _____/\\\\\\\\\ _______/\\__/\\ ___/\\\\\\\\\\\\\\\        
 __\/\\\/////////\\\ _/\\\///////\\\ ____\//__\//____\/\\\///////////__       
 ___\/\\\_______\/\\\_\/\\\_____\/\\\ ________________\/\\\ ____________      
@@ -12,10 +12,8 @@ _________\/// _____________\/// _______\/// __\///////// __\/// _____________
 
 /*-----------------------------------------------------------------
  *  TO DO
- *
+ *  Custodial Protocol Functions
  *---------------------------------------------------------------*/
-
-//CTS:EXAMINE quick explainer for the contract
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
@@ -23,10 +21,9 @@ pragma solidity ^0.8.0;
 import "./PRUF_CORE.sol";
 
 contract NP is CORE {
-    /*
+    /**
      * @dev Verify user credentials
-     * //CTS:EXAMINE param
-     * Originating Address: //CTS:EXAMINE??
+     * @param _idxHash asset to check that caller holds token
      */
     modifier isAuthorized(bytes32 _idxHash) override {
         uint256 tokenId = uint256(_idxHash);
@@ -39,24 +36,17 @@ contract NP is CORE {
 
     //--------------------------------------------External Functions--------------------------
 
-    /*
-     * @dev Modify **Record**.assetStatus with confirmation required
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE returns
+    /**
+     * @dev Modify **Record**.assetStatus with confirmation of matching rgthash required
+     * @param _idxHash asset to moidify
+     * @param _rgtHash rgthash to match in front end
+     * @param _newAssetStatus updated status
      */
     function _modStatus(
         bytes32 _idxHash,
         bytes32 _rgtHash,
         uint8 _newAssetStatus
-    )
-        external
-        nonReentrant
-        whenNotPaused
-        isAuthorized(_idxHash)
-        returns (uint8)
-    {
+    ) external nonReentrant whenNotPaused isAuthorized(_idxHash) {
         Record memory rec = getRecord(_idxHash);
         uint8 userType = getCallingUserType(rec.assetClass);
 
@@ -90,29 +80,20 @@ contract NP is CORE {
         //^^^^^^^effects^^^^^^^^^
 
         writeRecord(_idxHash, rec);
-
-        return rec.assetStatus;
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    /*
-     * @dev set **Record**.assetStatus to lost or stolen, with confirmation required. //CTS:EXAMINE with confirmation?
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE returns
+    /**
+     * @dev set **Record**.assetStatus to lost or stolen, with confirmation of matching rgthash required
+     * @param _idxHash asset to moidify
+     * @param _rgtHash rgthash to match in front end
+     * @param _newAssetStatus updated status
      */
     function _setLostOrStolen(
         bytes32 _idxHash,
         bytes32 _rgtHash,
         uint8 _newAssetStatus
-    )
-        external
-        nonReentrant
-        whenNotPaused
-        isAuthorized(_idxHash)
-        returns (uint8)
-    {
+    ) external nonReentrant whenNotPaused isAuthorized(_idxHash) {
         Record memory rec = getRecord(_idxHash);
         uint8 userType = getCallingUserType(rec.assetClass);
         require((userType > 0) && (userType < 10), "NP:SLS: User !auth in AC");
@@ -131,29 +112,20 @@ contract NP is CORE {
         //^^^^^^^effects^^^^^^^^^
 
         STOR.setLostOrStolen(_idxHash, rec.assetStatus);
-
-        return rec.assetStatus;
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    /*
-     * @dev Decrement **Record**.countdown with confirmation required
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE returns
+    /**
+     * @dev Decrement **Record**.countdown with confirmation of matching rgthash required
+     * @param _idxHash asset to moidify
+     * @param _rgtHash rgthash to match in front end
+     * @param _decAmount amount to decrement
      */
     function _decCounter(
         bytes32 _idxHash,
         bytes32 _rgtHash,
         uint32 _decAmount
-    )
-        external
-        nonReentrant
-        whenNotPaused
-        isAuthorized(_idxHash)
-        returns (uint32)
-    {
+    ) external nonReentrant whenNotPaused isAuthorized(_idxHash) {
         Record memory rec = getRecord(_idxHash);
         uint8 userType = getCallingUserType(rec.assetClass);
 
@@ -177,16 +149,15 @@ contract NP is CORE {
 
         writeRecord(_idxHash, rec);
         deductServiceCosts(rec.assetClass, 7);
-        return (rec.countDown);
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    /*
+    /**
      * @dev Modify **Record**.Ipfs1
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
-     * //CTS:EXAMINE param
+     * @param _idxHash idx of asset to Modify
+     * @param _rgtHash rgthash to match in front end
+     * @param _Ipfs1a content adressable storage adress part 1
+     * @param _Ipfs1b content adressable storage adress part 2
      */
     function _modIpfs1(
         bytes32 _idxHash,
@@ -216,7 +187,7 @@ contract NP is CORE {
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    /*
+    /**
      * @dev Export FROM Custodial - sets asset to status 70 (importable) //CTS:EXAMINE we should maybe describe this better
      * //CTS:EXAMINE param
      * //CTS:EXAMINE param
@@ -226,13 +197,7 @@ contract NP is CORE {
         bytes32 _idxHash,
         uint32 _exportTo,
         address _addr
-    )
-        external
-        nonReentrant
-        whenNotPaused
-        isAuthorized(_idxHash)
-        returns (uint8)
-    {
+    ) external nonReentrant whenNotPaused isAuthorized(_idxHash) {
         Record memory rec = getRecord(_idxHash);
         uint8 userType = getCallingUserType(rec.assetClass);
         //AC memory AC_info = getACinfo(rec.assetClass); //CTS:EXAMINE untested dont delete
@@ -242,7 +207,7 @@ contract NP is CORE {
             "NP:EA: user not auth in AC"
         );
         require( // require transferrable (51) status
-            rec.assetStatus == 51,
+            (rec.assetStatus == 51) || (rec.assetStatus == 70), //DPS:check
             "NP:EA: Asset status must be 51 to export"
         );
         require(
@@ -257,9 +222,5 @@ contract NP is CORE {
 
         APP.transferAssetToken(_addr, _idxHash);
         writeRecord(_idxHash, rec);
-        //STOR.changeAC(_idxHash, AC_info.assetClassRoot); //CTS:EXAMINE untested dont delete
-
-        return rec.assetStatus;
-        //^^^^^^^interactions^^^^^^^^^
     }
 }
