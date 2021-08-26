389b6b05de86f0df755835f2fdff8b25ac490a81
Cliff <cliff.smyth@gmail.com>
2021-08-26 17:36:46 -0400

    functional and ready to begin testing

--- PRUF_TRUFFLE/contracts/PRUF_CONSIGNMENT_MARKET.sol
+++ PRUF_TRUFFLE/contracts/PRUF_CONSIGNMENT_MARKET.sol
@@ -14,8 +14,8 @@ _________\/// _____________\/// _______\/// __\///////// __\/// _____________
  *  TO DO ---
  *
  *-----------------------------------------------------------------
- * Wraps and unwraps ERC721 compliant tokens in a PRUF Asset token
- *  MUST BE TRUSTED AGENT IN CNSGN_TKN
+ * Wraps and unwraps ERC721 compliant tokens in a PRUF Asset token  DPS:NEW CONTRACT DPS:CHECK
+ *  MUST BE TRUSTED AGENT IN CNSGN_TKN, Atkn, Util_Tkn
  *----------------------------------------------------------------*/
 
 // SPDX-License-Identifier: UNLICENSED
@@ -23,16 +23,18 @@ pragma solidity ^0.8.6;
 
 import "./PRUF_CORE.sol";
 import "./Imports/token/ERC721/IERC721.sol";
+import "./Imports/token/ERC20/IERC20.sol";
 
 contract Market is CORE {
     struct ConsignmentTags {
-        uint256 tokenID;
+        uint256 tokenId;
         address tokenContract;
         address currency;
         uint256 price;
     }
 
     address internal CNSGN_TKN_Address;
+    address public charityAddress;
     CNSGN_TKN_Interface internal CNSGN_TKN;
 
     mapping(uint256 => ConsignmentTags) private wrapped; // pruf tokenID -> original TokenID, ContractAddress
@@ -53,10 +55,21 @@ contract Market is CORE {
     }
 
     //--------------------------------------------External Functions--------------------------
+    /**
+     * @dev sets the charity donation address
+     * @param _charityAddress charity erc20 address
+     */
+
+    function setCharityAddress(address _charityAddress)
+        external
+        isContractAdmin
+    {
+        charityAddress = _charityAddress;
+    }
 
     /**
      * @dev Wraps a token, takes original from caller
-     * @param _foreignTokenID tokenID of token to wrap
+     * @param _foreignTokenId tokenID of token to wrap
      * @param _foreignTokenContract contract address for token to wrap
      * @param _currency currency to make transaction in
      * @param _price price in _currency to require for transfer
@@ -68,41 +81,51 @@ contract Market is CORE {
      * referenceAddress must be '0' or ERC721 contract address
      *
      */
-    function consign(uint256 _foreignTokenID, address _foreignTokenContract, address _currency, uint256 _price)
+    function consign(
+        uint256 _foreignTokenId,
+        address _foreignTokenContract,
+        address _currency,
+        uint256 _price
+    )
         external
         nonReentrant
         whenNotPaused
-        isTokenHolder(_foreignTokenID, _foreignTokenContract) 
-    { // without this, the dark forest gets it!
+        isTokenHolder(_foreignTokenId, _foreignTokenContract)
+    {
+        // without this, the dark forest gets it!
         bytes32 consignmentTag = keccak256(
-            abi.encodePacked(_foreignTokenID, _foreignTokenContract)
+            abi.encodePacked(_foreignTokenId, _foreignTokenContract)
         );
 
         if (_foreignTokenContract == A_TKN_Address) {
-            bytes32 idxHash = bytes32(_foreignTokenID);
+            bytes32 idxHash = bytes32(_foreignTokenId);
             Record memory rec = getRecord(idxHash);
             require(
                 rec.assetStatus == 51,
                 "CM:C:PRUF asset is not status 51 (transferrable)"
             );
-            A_TKN.trustedAgentTransferFrom(_msgSender(), address(this), _foreignTokenID); //takes PRUF asset usint TRUSTED_AGENT_ROLE
+            A_TKN.trustedAgentTransferFrom(
+                _msgSender(),
+                address(this),
+                _foreignTokenId
+            ); //takes PRUF asset usint TRUSTED_AGENT_ROLE
         }
 
         uint256 newTokenId = uint256(consignmentTag);
 
         //^^^^^^^checks^^^^^^^^^
 
-        wrapped[newTokenId].tokenID = _foreignTokenID;
+        wrapped[newTokenId].tokenId = _foreignTokenId;
         wrapped[newTokenId].tokenContract = _foreignTokenContract;
         wrapped[newTokenId].currency = _currency;
         wrapped[newTokenId].price = _price;
         //^^^^^^^effects^^^^^^^^^
 
-        foreignTransfer(
+        foreign721Transfer(
             _foreignTokenContract,
             _msgSender(),
             address(this),
-            _foreignTokenID
+            _foreignTokenId
         ); // move token to this contract
 
         CNSGN_TKN.mintConsignmentToken(
@@ -116,23 +139,24 @@ contract Market is CORE {
 
     /**
      * @dev Unwraps a token, returns original to caller
-     * @param _tokenID tokenID of PRUF token being unwrapped
+     * @param _tokenId tokenID of PRUF token being unwrapped
      * burns pruf asset from caller wallet
      * Sends original 721 to caller
      */
-    function withdrawFromConsignment(uint256 _tokenID)
+    function withdrawFromConsignment(uint256 _tokenId)
         external
         nonReentrant
         whenNotPaused
-        isTokenHolder(_tokenID, CNSGN_TKN_Address)
-    {    //caller holds the consignment ticket
-        address foreignTokenContract = wrapped[_tokenID].tokenContract;
-        uint256 foreignTokenID = wrapped[_tokenID].tokenID;
+        isTokenHolder(_tokenId, CNSGN_TKN_Address)
+    {
+        //caller holds the consignment ticket ^^
+        address foreignTokenContract = wrapped[_tokenId].tokenContract;
+        uint256 foreignTokenID = wrapped[_tokenId].tokenId;
         //^^^^^^^checks^^^^^^^^^
 
-        CNSGN_TKN.trustedAgentBurn(_tokenID);
+        CNSGN_TKN.trustedAgentBurn(_tokenId);
 
-        foreignTransfer(
+        foreign721Transfer(
             foreignTokenContract,
             address(this),
             _msgSender(),
@@ -141,40 +165,69 @@ contract Market is CORE {
         //^^^^^^^interactions^^^^^^^^^
     }
 
-    // /**
-    //  * @dev Purchse an item in transferrable status with price and currency set to pruf
-    //  * @param _idxHash asset ID
-    //  */
-    // function purchaseWithPRUF(
-    //     bytes32 _idxHash
-    // ) external whenNotPaused
-    // {
-    //     Record memory rec = getRecord(_idxHash);
-    //     (rec.price, rec.currency) = STOR.getPriceData(_idxHash);
+    /*
+     * @dev Purchse an item in transferrable status with price and currency set to pruf
+     * @param _idxHash asset ID
+     */
+    function purchase(
+        uint256 _tokenId //consignment token ID
+    ) external whenNotPaused {
+        require( // this is redundant, will throw upon transfer
+            (IERC721(wrapped[_tokenId].tokenContract).ownerOf(
+                wrapped[_tokenId].tokenId
+            ) == address(this)),
+            "CM:P: Market contract does not hold specified token"
+        );
 
-    //     uint256 tokenId = uint256(_idxHash);
-    //     address assetHolder = A_TKN.ownerOf(tokenId);
+        address paymentAddress;
 
-    //     require(
-    //         rec.assetStatus == 51,
-    //         "PP:P: Must be in transferrable status (51)"
-    //     );
-    //     require(
-    //         rec.currency == 2,
-    //         "PP:P: Payment must be in PRUF tokens for this contract"
-    //     );
-    //     //^^^^^^^checks^^^^^^^^^
+        if (CNSGN_TKN.tokenExists(_tokenId) != 0) {
+            //if ticket !exist, sale will go through and proceeds will go to the charity address.
+            CNSGN_TKN.trustedAgentBurn(_tokenId); //burn the consignment tag, consignment is over as sale is being completed
+            paymentAddress = CNSGN_TKN.ownerOf(wrapped[_tokenId].tokenId); //set payment address to the holder of the consignment ticket
+        } else {
+            //otherwise
+            paymentAddress = charityAddress; //set payment address to the charity address
+        }
 
-    //     // --- transfer the PRUF tokens
-    //     if (rec.price > 0) {
-    //         // allow for freeCycling
-    //         UTIL_TKN.trustedAgentTransfer(_msgSender(), assetHolder, rec.price);
-    //     }
-    //     // --- transfer the asset token
-    //     A_TKN.trustedAgentTransferFrom(assetHolder, _msgSender(), tokenId);
-    //     deductServiceCosts(rec.node, 2);
-    //     //^^^^^^^interactions^^^^^^^^^
-    // }
+        if (wrapped[_tokenId].tokenContract == A_TKN_Address) {
+            //If token is a PRüF asset
+            A_TKN.trustedAgentTransferFrom( //Deliver using TAT
+                address(this), //from this address
+                _msgSender(), //to the purchase caller
+                _tokenId //send this PRüF token
+            );
+        } else {
+            //otherwise
+            foreign721Transfer( //Deliver using allowance
+                wrapped[_tokenId].tokenContract, //using this token contract
+                address(this), //from this address
+                _msgSender(), //to the purchase caller
+                wrapped[_tokenId].tokenId //send this (native) tokenId
+            );
+        }
+
+        if (
+            //payment currency is PRUF and wallet is permitted for TAT
+            (wrapped[_tokenId].currency == UTIL_TKN_Address) &&
+            (UTIL_TKN.isColdWallet(_msgSender()) == 0)
+        ) {
+            //Pay in PRüF using TAT
+            UTIL_TKN.trustedAgentTransfer(
+                _msgSender(),
+                paymentAddress,
+                wrapped[_tokenId].price
+            );
+        } else {
+            //otherwise
+            foreign20Transfer( //Pay using allowance
+                wrapped[_tokenId].currency, //send this erc20
+                _msgSender(), //from the purchase caller
+                paymentAddress, //to the payment address
+                wrapped[_tokenId].price //amount of tokens to send
+            );
+        }
+    }
 
     /**
      * @dev transfer a foreign token
@@ -183,12 +236,28 @@ contract Market is CORE {
      * @param _to destination
      * @param _tokenID Token ID
      */
-    function foreignTransfer(
+    function foreign721Transfer(
         address _tokenContract,
         address _from,
         address _to,
         uint256 _tokenID
     ) internal {
         IERC721(_tokenContract).transferFrom(_from, _to, _tokenID);
+    }
+
+    /**
+     * @dev transfer a foreign token
+     * @param _tokenContract Address of foreign token contract
+     * @param _from origin
+     * @param _to destination
+     * @param _amount amount to transfer
+     */
+    function foreign20Transfer(
+        address _tokenContract,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        IERC20(_tokenContract).transferFrom(_from, _to, _amount);
     }
 }
