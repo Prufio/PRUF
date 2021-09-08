6f9cb9bbcfcb897a0a6341c701d7cdeda6cb9f64
Drake Smyth <drakesmyth@gmail.com>
2021-09-08 15:47:28 -0400

    flattened tokens

--- PRUF_TRUFFLE/contracts/PRUF_STAKE_TKN.sol
+++ PRUF_TRUFFLE/contracts/PRUF_STAKE_TKN.sol
@@ -20,10 +20,8 @@ pragma solidity ^0.8.6;
 
 import "./PRUF_INTERFACES.sol";
 import "./Imports/access/AccessControl.sol";
-import "./Imports/utils/Context.sol";
 import "./Imports/utils/Counters.sol";
 import "./Imports/utils/ReentrancyGuard.sol";
-import "./Imports/token/ERC721/ERC721.sol";
 import "./Imports/token/ERC721/ERC721Burnable.sol";
 import "./Imports/token/ERC721/ERC721Pausable.sol";
 
