310b263c98f44c7ea1a55186dbcabe934567e6c8
Cliff <cliff.smyth@gmail.com>
2022-01-25 14:26:39 -0400

    Added roles and Paused()

--- PRUF_TRUFFLE/contracts/InProgress/PRUF_DAO.sol
+++ PRUF_TRUFFLE/contracts/InProgress/PRUF_DAO.sol
@@ -36,6 +36,128 @@ contract DAO is BASIC {
         _;
     }
 
+    //   interface IAccessControl {
+
+    /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
+    function DAOhasRole(
+        bytes32 _role,
+        address _account,
+        string calldata _contract
+    ) external view returns (bool) {
+        return (
+            BASIC_Interface(resolveName(_contract)).hasRole(_role, _account)
+        );
+    }
+
+    /**
+     * @dev Returns the admin role that controls `role`. See {grantRole} and
+     * {revokeRole}.
+     *
+     * To change a role's admin, use {AccessControl-_setRoleAdmin}.
+     */
+    function DAOgetRoleAdmin(bytes32 _role, string calldata _contract)
+        external
+        view
+        returns (bytes32)
+    {
+        return (BASIC_Interface(resolveName(_contract)).getRoleAdmin(_role));
+    }
+
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
+    function DAOgrantRole(
+        bytes32 _role,
+        address _account,
+        string calldata _contract
+    ) external isDAOadmin {
+        BASIC_Interface(resolveName(_contract)).grantRole(_role, _account);
+    }
+
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
+    function DAOrevokeRole(
+        bytes32 _role,
+        address _account,
+        string calldata _contract
+    ) external isDAOadmin {
+        BASIC_Interface(resolveName(_contract)).revokeRole(_role, _account);
+    }
+
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been granted `role`, emits a {RoleRevoked}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `account`.
+     */
+    function DAOrenounceRole(
+        bytes32 _role,
+        address _account,
+        string calldata _contract
+    ) external isDAOadmin {
+        BASIC_Interface(resolveName(_contract)).renounceRole(_role, _account);
+    }
+
+    /**
+     * @dev Returns one of the accounts that have `role`. `index` must be a
+     * value between 0 and {getRoleMemberCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleMember} and {getRoleMemberCount}, make sure
+     * you perform all queries on the same block. See the following
+     * https://forum.openzeppelin.com/t/iterating-over-elements-on-enumerableset-in-openzeppelin-contracts/2296[forum post]
+     * for more information.
+     */
+    function DAOgetRoleMember(
+        bytes32 _role,
+        uint256 _index,
+        string calldata _contract
+    ) external view returns (address) {
+        return (
+            BASIC_Interface(resolveName(_contract)).getRoleMember(_role, _index)
+        );
+    }
+
+    /**
+     * @dev Returns the number of accounts that have `role`. Can be used
+     * together with {getRoleMember} to enumerate all bearers of a role.
+     */
+    function DAOgetRoleMemberCount(bytes32 _role, string calldata _contract)
+        external
+        view
+        returns (uint256)
+    {
+        return (
+            BASIC_Interface(resolveName(_contract)).getRoleMemberCount(_role)
+        );
+    }
+
     /**
      * @dev Resolve contract addresses from STOR
      * @param _contract contract name to call
@@ -75,6 +197,13 @@ contract DAO is BASIC {
      */
     function DAOunpause(string calldata _contract) external isDAOadmin {
         BASIC_Interface(resolveName(_contract)).unpause();
+    }
+
+    /**
+     * @dev Returns true if _contract is paused, and false otherwise.
+     */
+    function DAOpaused(string calldata _contract) external returns (bool) {
+        return (BASIC_Interface(resolveName(_contract)).paused());
     }
 
     /**
