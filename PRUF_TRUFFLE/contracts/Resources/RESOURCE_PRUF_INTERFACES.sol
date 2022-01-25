79a9dccebfc5777f5c090183fceb245e97ea340c
Cliff <cliff.smyth@gmail.com>
2022-01-25 14:26:25 -0400

    Added Roles and paused()

--- PRUF_TRUFFLE/contracts/Resources/RESOURCE_PRUF_INTERFACES.sol
+++ PRUF_TRUFFLE/contracts/Resources/RESOURCE_PRUF_INTERFACES.sol
@@ -1749,6 +1749,84 @@ interface UD_721_Interface {
  */
 interface BASIC_Interface {
     /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
+    function hasRole(bytes32 role, address account)
+        external
+        view
+        returns (bool);
+
+    /**
+     * @dev Returns the admin role that controls `role`. See {grantRole} and
+     * {revokeRole}.
+     *
+     * To change a role's admin, use {AccessControl-_setRoleAdmin}.
+     */
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
+    function grantRole(bytes32 role, address account) external;
+
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     */
+    function revokeRole(bytes32 role, address account) external;
+
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been granted `role`, emits a {RoleRevoked}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `account`.
+     */
+    function renounceRole(bytes32 role, address account) external;
+
+    /**
+     * @dev Returns one of the accounts that have `role`. `index` must be a
+     * value between 0 and {getRoleMemberCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleMember} and {getRoleMemberCount}, make sure
+     * you perform all queries on the same block. See the following
+     * https://forum.openzeppelin.com/t/iterating-over-elements-on-enumerableset-in-openzeppelin-contracts/2296[forum post]
+     * for more information.
+     */
+    function getRoleMember(bytes32 role, uint256 index)
+        external
+        view
+        returns (address);
+
+    /**
+     * @dev Returns the number of accounts that have `role`. Can be used
+     * together with {getRoleMember} to enumerate all bearers of a role.
+     */
+    function getRoleMemberCount(bytes32 role) external view returns (uint256);
+
+    /**
      * @dev Resolve contract addresses from STOR
      */
     function resolveContractAddresses() external;
@@ -1768,6 +1846,11 @@ interface BASIC_Interface {
      * @dev Returns to normal state. (pausable)
      */
     function unpause() external;
+
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
+    function paused() external returns (bool);
 
     /**
      * @dev send an ERC721 token from this contract
@@ -1793,3 +1876,88 @@ interface BASIC_Interface {
         uint256 _amount
     ) external;
 }
+
+//---------------------------------------------------------------------------------------------------------------
+
+// /**
+//  * @dev External interface of AccessControl declared to support ERC165 detection.
+//  */
+// interface IAccessControl {
+//     /**
+//      * @dev Returns `true` if `account` has been granted `role`.
+//      */
+//     function hasRole(bytes32 role, address account)
+//         external
+//         view
+//         returns (bool);
+
+//     /**
+//      * @dev Returns the admin role that controls `role`. See {grantRole} and
+//      * {revokeRole}.
+//      *
+//      * To change a role's admin, use {AccessControl-_setRoleAdmin}.
+//      */
+//     function getRoleAdmin(bytes32 role) external view returns (bytes32);
+
+//     /**
+//      * @dev Grants `role` to `account`.
+//      *
+//      * If `account` had not been already granted `role`, emits a {RoleGranted}
+//      * event.
+//      *
+//      * Requirements:
+//      *
+//      * - the caller must have ``role``'s admin role.
+//      */
+//     function grantRole(bytes32 role, address account) external;
+
+//     /**
+//      * @dev Revokes `role` from `account`.
+//      *
+//      * If `account` had been granted `role`, emits a {RoleRevoked} event.
+//      *
+//      * Requirements:
+//      *
+//      * - the caller must have ``role``'s admin role.
+//      */
+//     function revokeRole(bytes32 role, address account) external;
+
+//     /**
+//      * @dev Revokes `role` from the calling account.
+//      *
+//      * Roles are often managed via {grantRole} and {revokeRole}: this function's
+//      * purpose is to provide a mechanism for accounts to lose their privileges
+//      * if they are compromised (such as when a trusted device is misplaced).
+//      *
+//      * If the calling account had been granted `role`, emits a {RoleRevoked}
+//      * event.
+//      *
+//      * Requirements:
+//      *
+//      * - the caller must be `account`.
+//      */
+//     function renounceRole(bytes32 role, address account) external;
+
+//     /**
+//      * @dev Returns one of the accounts that have `role`. `index` must be a
+//      * value between 0 and {getRoleMemberCount}, non-inclusive.
+//      *
+//      * Role bearers are not sorted in any particular way, and their ordering may
+//      * change at any point.
+//      *
+//      * WARNING: When using {getRoleMember} and {getRoleMemberCount}, make sure
+//      * you perform all queries on the same block. See the following
+//      * https://forum.openzeppelin.com/t/iterating-over-elements-on-enumerableset-in-openzeppelin-contracts/2296[forum post]
+//      * for more information.
+//      */
+//     function getRoleMember(bytes32 role, uint256 index)
+//         external
+//         view
+//         returns (address);
+
+//     /**
+//      * @dev Returns the number of accounts that have `role`. Can be used
+//      * together with {getRoleMember} to enumerate all bearers of a role.
+//      */
+//     function getRoleMemberCount(bytes32 role) external view returns (uint256);
+//}
