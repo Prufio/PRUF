18b6de1f8154025f172aeaf6555e08a4b7bfd4d2
Cliff <cliff.smyth@gmail.com>
2022-03-04 16:01:17 -0400

    Implemented  faux-DAO

--- PRUF_TRUFFLE/contracts/InProgress/PRUF_DAO_.sol
+++ PRUF_TRUFFLE/contracts/InProgress/PRUF_DAO_.sol
@@ -19,14 +19,20 @@ _________\/// _____________\/// _______\/// __\///////// __\/// _____________
 pragma solidity 0.8.7;
 
 import "../Resources/PRUF_CORE.sol";
-import "../Resources/RESOURCE_PRUF_DAO-LAYER_INTERFACES.sol";
+import "../Resources/RESOURCE_PRUF_DAO_INTERFACES.sol";
 
 contract DAO is BASIC {
-
-    mapping (bytes32 => uint256) private motions;
+    uint256 quorum = 1;
+    uint256 passingPercentage = 60;
+    uint256 votingPeriod = 10 days;
+    uint256 confirmationPeriod = 2 days;
 
     bytes32 public constant DAO_ADMIN_ROLE = keccak256("DAO_ADMIN_ROLE");
     bytes32 public constant DAO_LAYER_ROLE = keccak256("DAO_LAYER_ROLE");
+
+    mapping(bytes32 => Motion) private motions;
+
+    event REPORT(bytes32 _motion, string _msg);
 
     /**
      * @dev Verify user credentials
@@ -54,31 +60,132 @@ contract DAO is BASIC {
         _;
     }
 
-    function createMotion(bytes32 motion) external {
-        motions[motion] = 1;
+    /**
+     * @dev Crates an new Motion in the motions map
+     * Originating Address:
+     *      holds > .9_ pruf
+     * @param _motion the hash of the referring contract address, function name, and parmaeters
+     */
+    function createMotion(bytes32 _motion) external {
+        require(
+            ((motions[_motion].status != 1) && (motions[_motion].status != 2)),
+            "DAO:CM:Motion exists - wait for or finalize motion in progress"
+        ); // Motion must not be currently proposed or approved
+        require(
+            UTIL_TKN.balanceOf(_msgSender()) > 99999999999999999, // 1 PRUF MINIMUM REQUIRED TO CREATE A MOTION
+            "DAO:CM:Proposer must hold =>1 PRUF"
+        );
+        //^^^^^^^checks^^^^^^^^^
+
+        delete motions[_motion]; //clear any existing data
+
+        motions[_motion].proposer = _msgSender();
+        motions[_motion].status = 1; //put motion in proposed status
+        motions[_motion].proposalTime = block.timestamp;
+        emit REPORT(_motion, "Motion Created");
+        //^^^^^^^effects^^^^^^^^^
     }
 
-    function adminAcceptMotion(bytes32 motion) external isDaoAdmin {
-        motions[motion] = 2;
+    /**
+     * @dev Admin voting : to be depricated ---------CAUTION:CENTRALIZATION RISK
+     * @param _motion the motion hash to be voted on
+     * @param _vote //1 = yea, 0 = neigh
+     */
+    function adminVote(bytes32 _motion, uint256 _vote) external isDAOadmin {
+        require(
+            motions[_motion].status == 1,
+            "DAO:AV:Motion not in 'proposed' status"
+        );
+        require(
+            block.timestamp <= motions[_motion].proposalTime + votingPeriod,
+            "DAO:AV:Voting window closed"
+        );
+        //^^^^^^^checks^^^^^^^^^
+
+        motions[_motion].voterCount++;
+        if (_vote == 1) motions[_motion].votes++;
+        //^^^^^^^effects^^^^^^^^^
     }
 
-    function 
+    /**
+     * @dev Finalizes / tallys votes for a mation
+     * @param _motion the motion hash to be finalized
+     * also clears any expired or failed motions
+     */
+    function finalizeVoting(bytes32 _motion) external {
+        if (
+            block.timestamp >=
+            motions[_motion].proposalTime + votingPeriod + confirmationPeriod
+        ) {
+            delete motions[_motion];
+            emit REPORT(_motion, "Motion Expired");
+            //^^^^^^^checks^^^^^^^^^
+        } else {
+            tallyVotes(_motion);
+        }
+        //^^^^^^^effects^^^^^^^^^
+    }
 
+    /**
+     * @dev Throws if a resolution is not approved. clears the motion if successful
+     * @param _motion the motion hash to check
+     * to be called by DAO_LAYER contracts as a check prior to executing functions
+     */
+    function verifyResolution(bytes32 _motion) external isDAOlayer {
+        require(
+            motions[_motion].status == 2,
+            "DAO:MOD-VR:specified action is not approved"
+        );
+        //^^^^^^^checks^^^^^^^^^
 
+        delete motions[_motion];
+        //^^^^^^^effects^^^^^^^^^
+    }
 
+    /**
+     * @dev Getter for motions
+     * @param _motion the motion hash to get
+     * to be called by DAO_LAYER contracts as a check prior to executing functions
+     */
+    function getMotionStatus(bytes32 _motion)
+        external
+        view
+        returns (Motion memory)
+    {
+        return motions[_motion];
+        //^^^^^^^interactions^^^^^^^^^
+    }
 
     //---------------------------------INTERNAL FUNCTIONS
 
-    // /**
-    //  * @dev name resolver
-    //  * @param _name name to resolve
-    //  * returns address of (contract name)
-    //  */
-    // function resolveName(string calldata _name)
-    //     internal
-    //     view
-    //     returns (address)
-    // {
-    //     return STOR.resolveContractAddress(_name);
-    // }
+    /**
+     * @dev Veriifies quorum and checks pass/fail for votes;
+     * deletes failed motions
+     * @param _motion the motion hash to tally
+     */
+    function tallyVotes(bytes32 _motion) internal {
+        require(
+            block.timestamp > motions[_motion].proposalTime + votingPeriod,
+            "DAO:FV:Voting still open, cannot finalize"
+        );
+        //^^^^^^^checks^^^^^^^^^
+
+        Motion memory thisMotion = motions[_motion];
+
+        if (thisMotion.voterCount > quorum) {
+            uint256 votePercent = ((thisMotion.votes * 100) /
+                thisMotion.voterCount);
+            if (votePercent >= passingPercentage) {
+                motions[_motion].status = 2;
+                emit REPORT(_motion, "Passed");
+            } else {
+                delete motions[_motion];
+                emit REPORT(_motion, "Failed");
+            }
+        } else {
+            delete motions[_motion];
+            emit REPORT(_motion, "Quorum not met.");
+        }
+        //^^^^^^^effects^^^^^^^^^
+    }
 }
